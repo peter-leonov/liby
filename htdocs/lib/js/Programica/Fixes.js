36819debfbae6815578762f01186f23686afc324
Peter Leonov <peter@kung-fu-tzu.ru>
2007-04-24 15:24:46 +0000

    + label

--- htdocs/lib/js/Programica/Fixes.js
+++ htdocs/lib/js/Programica/Fixes.js
@@ -7,21 +7,20 @@ Programica.Fixes =
 	{
 		this.runtimeStyle.behavior = 'none'
 		
-		Programica.Fixes.fixTitle.apply(this)
 		Programica.Fixes.fixPrototype.apply(this)
+		Programica.Fixes.fixTitle.apply(this)
 		
 		if (/MSIE 6/.test(navigator.userAgent))
 		{
 			Programica.Fixes.fixPng.apply(this)
 			Programica.Fixes.fixTitle.apply(this)
 			Programica.Fixes.fixLabel.apply(this)
-			Programica.Fixes.fixPrototype.apply(this)
+			
 		}
 		
 		if (/MSIE 7/.test(navigator.userAgent))
 		{
 			Programica.Fixes.fixTitle.apply(this)
-			Programica.Fixes.fixPrototype.apply(this)
 		}
 	},
 	
@@ -34,7 +33,6 @@ Programica.Fixes =
 		if (/\.png$/.test(this.src))
 		{
 			this.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + this.src + "', sizingMethod='image')"
-			//this.style.visibility = 'hidden'
 			this.setAttribute('src', '/lib/img/dot.gif')
 		}
 	},
@@ -48,7 +46,8 @@ Programica.Fixes =
 	/* делает кликабельными метки */
 	fixLabel: function ()
 	{
-		//this.attachEvent("onclick", function () {this.getElementsByTagName('input')[0] && this.getElementsByTagName('input')[0].click()})
+		var t = this
+		this.attachEvent("onclick", function () { var node = t.getElementsByTagName('input')[0]; if (node) node.click() })
 	},
 	
 	/* добавляет методы и свойства из HTMLElement.prototype */
