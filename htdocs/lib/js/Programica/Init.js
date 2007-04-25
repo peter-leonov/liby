34712f2813d29fed8e46dafe233cb56bb76fed66
Peter Leonov <peter@kung-fu-tzu.ru>
2007-04-25 12:42:08 +0000

    + прикольчеги для фотогалереи

--- htdocs/lib/js/Programica/Init.js
+++ htdocs/lib/js/Programica/Init.js
@@ -3,8 +3,9 @@ var isDebug = 0
 
 // заранее объявляем «пространства имен»
 
-var Programica = {}
-Programica.Abstract = {}
+if (!window.Programica) var Programica = {}
+if (!window.Programica.Abstract) Programica.Abstract = {}
+if (!window.Programica.DOM) Programica.DOM = {}
 
 
 //——————————————————————————————————————————————————————————————————————————————
@@ -31,6 +32,94 @@ function log (str)
 
 if (!window.HTMLElement) window.HTMLElement = {}
 if (!HTMLElement.prototype) HTMLElement.prototype = window["[[DOMElement.prototype]]"] || {}
+
+
+//——————————————————————————————————————————————————————————————————————————————
+// DOM для всех
+
+// took from http://muffinresearch.co.uk/archives/2006/04/29/getelementsbyclassname-deluxe-edition/
+Programica.DOM.getElementsByClassName = function (strClass, strTag)
+{
+	strTag = strTag || "*";
+	
+	var objColl = this.getElementsByTagName(strTag);
+	if (!objColl.length && strTag == "*" && this.all) objColl = this.all;
+	
+	var arr = new Array();
+	var delim = strClass.indexOf('|') != -1  ? '|' : ' ';
+	var arrClass = strClass.split(delim);
+	
+	for (var i = 0, ilen = objColl.length; i < ilen; i++)
+	{
+		var arrObjClass = objColl[i].className.split(' ');
+		if (delim == ' ' && arrClass.length > arrObjClass.length) continue;
+		
+		var c = 0;
+		comparisonLoop:
+		for (var k = 0, klen = arrObjClass.length; k < klen; k++)
+		{
+			for (var m = 0, mlen = arrClass.length; m < mlen; m++)
+			{
+				if (arrClass[m] == arrObjClass[k]) c++;
+				if (( delim == '|' && c == 1) || (delim == ' ' && c == arrClass.length))
+				{
+					arr.push(objColl[i]);
+					break comparisonLoop;
+				}
+			}
+		}
+	}
+	return arr;
+}
+
+Programica.DOM.getElementsByName = function (strName, strTag)
+{
+	strTag = strTag || "*";
+	
+	var objColl = this.getElementsByTagName(strTag);
+	if (!objColl.length && strTag == "*" && this.all) objColl = this.all;
+	
+	var arr = new Array();
+	var delim = strName.indexOf('|') != -1  ? '|' : ' ';
+	var arrName = strName.split(delim);
+	
+	for (var i = 0, ilen = objColl.length; i < ilen; i++)
+	{
+		var objName = objColl[i].getAttribute('name');
+		if (!objName) continue;
+		
+		var c = 0;
+		comparisonLoop:
+		for (var m = 0, mlen = arrName.length; m < mlen; m++)
+		{
+			if (arrName[m] == objName) c++;
+			if (( delim == '|' && c == 1) || (delim == ' ' && c == arrName.length))
+			{
+				arr.push(objColl[i]);
+				break comparisonLoop;
+			}
+		}
+	}
+	return arr;
+}
+
+
+if (!HTMLElement.prototype.getElementsByClassName)
+	HTMLElement.prototype.getElementsByClassName = Programica.DOM.getElementsByClassName
+
+if (!document.getElementsByClassName)
+	document.getElementsByClassName = Programica.DOM.getElementsByClassName
+
+if (!HTMLElement.prototype.getElementsByName)
+	HTMLElement.prototype.getElementsByName = Programica.DOM.getElementsByName
+
+if (!document.getElementsByName)
+	document.getElementsByName = Programica.DOM.getElementsByName
+
+/* кривоватый фикс addEventListener(...) для IE */
+if (!window.addEventListener)
+	HTMLElement.prototype.addEventListener = function (type, func, dir) { this.attachEvent('on' + type, func) },
+	window.addEventListener = document.addEventListener = HTMLElement.prototype.addEventListener
 
 
 //——————————————————————————————————————————————————————————————————————————————
@@ -92,7 +181,9 @@ function inherit (to, from)
 	return to
 }
 
-function $(id) { return document.getElementById(id) }
+function $ (id) { return document.getElementById(id) }
+
+if (!window.console) console = { log: function () {} }
 
 
 //——————————————————————————————————————————————————————————————————————————————
--- htdocs/lib/js/Programica/RollingImages.js
+++ htdocs/lib/js/Programica/RollingImages.js
@@ -1,8 +1,5 @@
-function $ (id) { return document.getElementById(id) }
 
-if (!window.console) console = { log:function(){} }
-
-if (!window.Programica) var Programica = {}
+window.addEventListener('load', function () { Programica.RollingImages.onLoader() }, false)
 
 Programica.RollingImages =
 {
@@ -10,7 +7,7 @@ Programica.RollingImages =
 	{
 		node.pmc || (node.pmc = {});
 		node.pmc.imageRoller = new Programica.RollingImages.Handler(node)
-		node.pmc.imageRoller.go(0)
+		node.pmc.imageRoller.goInit()
 	},
 	
 	Handler: function (node)
@@ -18,13 +15,13 @@ Programica.RollingImages =
 		this.node = node
 		var t = this
 		
-		this.viewport = node.getElementsByTagName('div')[0]
-		this.images = node.getElementsByTagName('img')
+		this.viewport = node.getElementsByClassName('viewport')[0]
+		this.images = node.getElementsByClassName('point')
 		this.current = 0
 		
 		var as = node.getElementsByTagName('a')
-		this.aPrev = as[0]
-		this.aNext = as[1]
+		this.aPrev = node.getElementsByClassName('prev')[0]
+		this.aNext = node.getElementsByClassName('next')[0]
 		this.aPrev.onclick = function () { t.goPrev() }
 		this.aNext.onclick = function () { t.goNext() }
 		
@@ -32,9 +29,10 @@ Programica.RollingImages =
 	
 	onLoader: function ()
 	{
-		var all = document.getElementsByClassName
+		var all = document.getElementsByClassName('programica-rolling-images')
 		
-		this.bind($("rolling-images1"))
+		for (var i = 0; i < all.length; i++)
+			this.bind(all[i])
 	}
 }
 
@@ -43,21 +41,42 @@ Programica.RollingImages.Handler.prototype =
 	goPrev: function () { if (this.current > 0) this.go((this.images.length + this.current - 1) % this.images.length) },
 	goNext: function () { if (this.current < this.images.length - 1) this.go((this.current + 1) % this.images.length) },
 	
+	animationType: function () { return this.node.getAttribute('animation-type') || 'easeOutBack' }, //easeOutBounce
+	
+	goInit: function (n)
+	{
+		n = n || 0
+		var img = this.images[n]
+		if (!img) return 0;
+		if (!this.viewport) log('Viewport is undefined!');
+		
+		this.viewport.scrollTop = img.offsetTop
+		this.viewport.scrollLeft = img.offsetLeft
+		
+		this.current = n
+		this.updateNavigation()
+		return n
+	},
+	
 	go: function (n)
 	{
 		var img = this.images[n]
 		if (!img) return 0
+		if (!this.viewport) log('Viewport is undefined!');
 		
-		log(n + '.offsetTop = ' + img.offsetTop)
+		log(n + ': offsetTop = ' + img.offsetTop + ', offsetLeft = ' + img.offsetLeft)
 		
-		this.viewport.animate('easeOutBounce', {scrollTop: [img.offsetTop]}, 1).start()
-		//this.viewport.scrollTop = img.offsetTop
-		
-		!n ? this.aPrev.className += ' disabled' : this.aPrev.className = this.aPrev.className.replace(/ disabled/g, '')
-		n == this.images.length - 1 ? this.aNext.className += ' disabled' : this.aNext.className = this.aNext.className.replace(/ disabled/g, '')
-		if (!n && n == this.images.length - 1) this.aPrev.className += ' disabled', this.aNext.className += ' disabled'
+		this.viewport.animate(this.animationType(), {scrollTop:  [img.offsetTop], scrollLeft: [img.offsetLeft]},  1).start()
 		
 		this.current = n
+		this.updateNavigation()
 		return n
+	},
+	
+	updateNavigation: function ()
+	{
+		!this.current ? this.aPrev.className += ' disabled' : this.aPrev.className = this.aPrev.className.replace(/ disabled/g, '')
+		this.current == this.images.length - 1 ? this.aNext.className += ' disabled' : this.aNext.className = this.aNext.className.replace(/ disabled/g, '')
+		if (!this.current && this.current == this.images.length - 1) this.aPrev.className += ' disabled', this.aNext.className += ' disabled'
 	}
 }
