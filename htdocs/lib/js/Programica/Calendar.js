b233d69a1acd3e3fcc8d936f7d22eeedbf563ac6
Peter Leonov <peter@kung-fu-tzu.ru>
2007-05-03 13:03:43 +0000

    календарь в процессе

---
+++ htdocs/lib/js/Programica/Calendar.js
@@ -0,0 +1,97 @@
+
+window.addEventListener('load', function () { Programica.Calendar.onLoader() }, false)
+
+Programica.Calendar =
+{
+	bind: function (node)
+	{
+		node.pmc || (node.pmc = {})
+		node.pmc.Calendar = new this.Handler(node)
+		node.pmc.Calendar.init()
+	},
+	
+	Handler: function (node)
+	{
+		this.mainNode = node
+		
+	},
+	
+	onLoader: function ()
+	{
+		var all = document.getElementsByClassName('programica-calendar')
+		
+		for (var i = 0; i < all.length; i++)
+			this.bind(all[i])
+	}
+}
+
+Programica.Calendar.Handler.prototype =
+{
+	init: function ()
+	{
+		var t = this
+		aGet(this.mainNode.getAttribute('calendar-href')).onLoad = function ()
+		{
+			t.dataLoaded(this)
+		}
+	},
+	
+	dataLoaded: function (r)
+	{
+		this.request = r
+		
+		var t = this
+		setTimeout(function () { t.draw() }, 10)
+	},
+	
+	draw: function ()
+	{
+		var data = this.parce(this.request.responseXML())
+		
+		log(data)
+	},
+	
+	parce: function (root)
+	{
+		var data = {}
+		
+		// руками раскладываем XML в хеш
+		var years = root.getElementsByTagName('year')
+		for (var yi = 0; yi < years.length; yi++)
+		{
+			var y = years[yi]
+			var ynum = y.getAttribute('num')
+			
+			var months = y.getElementsByTagName('month')
+			for (var mi = 0; mi < months.length; mi++)
+			{
+				m = months[mi]
+				var mnum = m.getAttribute('num')
+				
+				var days = m.getElementsByTagName('day')
+				for (var di = 0; di < days.length; di++)
+				{
+					var d = days[di]
+					var dnum = d.getAttribute('num')
+					
+					var date = new Date(0)
+					date.setFullYear(ynum, mnum - 1, dnum)
+					
+					//log(ynum + "-" + mnum + "-" + dnum + ": " + date.iso() + date)
+					data[date] = d
+				}
+			}
+		}
+		
+		return data
+	}
+}
+
+Date.prototype.fixZ = function (d) { if (d == 0) return "00"; if (d < 10) return "0" + d; return d }
+
+Date.prototype.iso = function ()
+{
+	// тупо, но быстро :)
+	with (this)
+		return (1900 + getYear()) + "-" + fixZ(getMonth()+1) + "-" + fixZ(getDate()) + " " + fixZ(getHours()) + ":" + fixZ(getMinutes()) + ":" + fixZ(getSeconds())
+}
