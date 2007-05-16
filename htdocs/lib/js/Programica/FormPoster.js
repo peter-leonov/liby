76340319d6c78be9fae408a0b727caadb3144eee
Peter Leonov <peter@kung-fu-tzu.ru>
2007-05-16 15:06:06 +0000

    форму принимает :)

--- htdocs/lib/js/Programica/FormPoster.js
+++ htdocs/lib/js/Programica/FormPoster.js
@@ -9,15 +9,29 @@ Programica.FormPoster.prototype.Handler = function (node)
 	this.mainNode = node
 	var t = this
 	
-	var listener = function (e)
+	var submit_listener = function (e)
 	{
+		// проверим, нужно ли ловить эту форму
 		if (!/^ajax$/i.test(this.getAttribute("target"))) return
 		
+		form = this
 		
+		// поиграем
+		Programica.FormPoster.bakeEvents(this, ["onload", "onsuccess", "onerror"])
+		
+		// собственно отправляем данные
+		with (aPost(this.action, form2hash(this)))
+		{
+			onLoad		= function () { form.onload({request:this}) }
+			onSuccess	= function () { form.onsuccess({request:this}) }
+			onError		= function () { form.onerror({request:this}) }
+		}
+		
+		// не даем форме отправиться (кое-как работает в ИЕ нашими стараниями)
 		e.preventDefault()
 	}
 	
-	node.addEventListener('submit', listener, false)
+	node.addEventListener('submit', submit_listener, false)
 }
 
 
@@ -25,5 +39,14 @@ Programica.FormPoster.prototype.Handler.prototype =
 {
 	init: function () { /*пусто*/ }
 }
+
+
+//Programica.FormPoster.code2event = function (code) { return eval("[function (event) { " + code + " }]")[0] }
+Programica.FormPoster.bakeEvents = function (node, events)
+{
+	for (var i = 0; i < events.length; i++)
+		node[events[i]] = eval("[function (event) { " + form.getAttribute(events[i]) + " }]")[0]
+}
+
 
 Programica.Widget.register(new Programica.FormPoster())
