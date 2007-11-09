ccd4962a0d08c0e46a4612fc743ee32750207d3b
Peter Leonov <peter@kung-fu-tzu.ru>
2007-11-09 16:19:46 +0000

    - * { fix(this) } + onload.fix(all) ~ faster fixes

--- htdocs/lib/Programica/IEFixes.js
+++ htdocs/lib/Programica/IEFixes.js
@@ -3,7 +3,7 @@ if (!self.Programica) Programica = {}
 if (!self.HTMLFormElement) self.HTMLFormElement = {prototype:{}}
 if (!self.Element) self.Element = {prototype:{}}
 
-with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype})
+with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(.+?)"\)$/})
 {
 	Programica.IEFixes =
 	{
@@ -26,52 +26,46 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype})
 			node.disabled ? node.addClassName('disabled') : node.remClassName('disabled')
 		},
 		
-		fixPng: function (node)
+		// отчасти исправляет обработку png
+		fixImgPng: function (node)
 		{
-			if (node.runtimeStyle.filter) return
-			if (node.tagName == 'IMG' && /\.png$/.test(node.src))
-			{
-				node.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + node.src + "', sizingMethod='image')"
-				node.setAttribute('src', '/lib/img/dot.gif')
-			}
-			else if (/url\("(.+\.a\.png)"\)$/.test(node.currentStyle.backgroundImage))
-			{
-				//alert(RegExp.$1)
-				node.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + RegExp.$1 + "', sizingMethod='crop')"
-				node.runtimeStyle.backgroundImage = 'none'
-				//node.width = node.width
-				//node.setAttribute('src', '/lib/img/dot.gif')
-			}
+
+			node.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + node.src + "', sizingMethod='image')"
+			node.setAttribute('src', '/lib/img/dot.gif')
+			//node.runtimeStyle.visibility = 'hidden'
 		},
-		
-		// убирает лишние подсказки
-		fixTitle: function (node)
+		fixBgPng: function (node)
 		{
-			if (!node.title) node.title = ''
+			// rex1 is static, so I cut it out to the with(...)
+			var m = rex1.exec(node.currentStyle.backgroundImage)
+			node.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + m[1] + "',sizingMethod='crop')"
+			node.runtimeStyle.backgroundImage = 'none'
 		},
 		
 		// делает кликабельными метки
 		fixLabel: function (node)
 		{
-			if (node.tagName != 'LABEL') return
-			
 			node.attachEvent
 			(
 				'onclick',
 				function ()
 				{
-					// трай/кеч нужен, иначе как надежно определить,
-					// может ли элемент принять фокус сейчас или нет
+					// трай/кеч нужен; иначе необходимо определить,
+					// может элемент принять фокус или нет
 					try
 					{
-						var n = (node.getElementsByTagName('input')[0] || node.getElementsByTagName('textarea')[0] || node.getElementsByTagName('select')[0])
-						if (n)
-						{
-							n.click()
-							n.focus()
-						}
+						if
+						(
+							n =
+							(
+								this.getElementsByTagName('input')[0] ||
+								this.getElementsByTagName('textarea')[0] ||
+								this.getElementsByTagName('select')[0]
+							)
+						)
+							n.click(), n.focus()
 					}
-					catch (ex) {}
+					catch (ex) { log(ex.message) }
 				}
 			)
 		},
@@ -87,17 +81,20 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype})
 				for (var p in hfe)
 					node[p] = hfe[p]
 			}
+		},
+		
+		fixThem: function (nodes)
+		{
+			for (var i = 0, il = nodes.length; i < il; i++)
+				fixIE(nodes[i])
 		}
 	}
 }
-//var str = {}
-//document.onclick = function () { alert(stringify(str)) }
+
 with (Programica.IEFixes)
 {
 	var onpropertychange6 = function ()
 	{
-		//str[event.propertyName] = 1
-		
 		if (event.propertyName == 'style.opacity')
 			fixOpacity(this)
 		else if (event.propertyName == 'disabled')
@@ -116,30 +113,13 @@ with (Programica.IEFixes)
 	
 	fixIE6 = function (node)
 	{
-		//node.runtimeStyle.behavior = 'none'
-		
 		fixPrototype(node)
-		/*fixOpacity(node)*/
-		fixPng(node)
-		fixTitle(node)
-		fixLabel(node)
-		fixTitle(node)
-		
-		node._pmcfix = true
-		
 		node.onpropertychange = onpropertychange6
 	}
 	
 	var fixIE7 = function (node)
 	{
-		//node.runtimeStyle.behavior = 'none'
-		
 		fixPrototype(node)
-		/*fixOpacity(node)*/
-		fixTitle(node)
-		
-		node._pmcfix = true
-		
 		node.onpropertychange = onpropertychange7
 	}
 	
@@ -147,45 +127,64 @@ with (Programica.IEFixes)
 		fixIE = fixIE7
 	else if (/MSIE 6/.test(navigator.userAgent))
 		fixIE = fixIE6
+	
+	fixIEImgPng = fixImgPng
+	fixIEBgPng = fixBgPng
 }
 
 
-
-Programica.IEFixes.fixThem = function (nodes)
-{
-	for (var i = 0, il = nodes.length; i < il; i++)
-		fixIE(nodes[i])
-}
 
 Programica.IEFixes.eventConversion = { DOMMouseScroll: 'mousewheel' }
 
-// кривоватый фикс addEventListener(...)
+Programica.IEFixes.preventDefault = function ()
+{
+	var old = this.returnValue
+	this.returnValue = false
+	return old
+}
+
+Programica.IEFixes.stopPropagation = function ()
+{
+	var old = this.cancelBubble
+	this.cancelBubble = true
+	return old
+}
+
+// yet another addEventListener(...) fix
 if (!self.addEventListener && self.attachEvent)
 {
 	Element.prototype.addEventListener = function (type, func, dir)
 	{
-		if (Programica.IEFixes.eventConversion[type])
-			type = Programica.IEFixes.eventConversion[type]
+		type = Programica.IEFixes.eventConversion[type] || type
 		
-		// JavaScript — сила! Каково: сохранить функцию-обертку в свойстве оборачиваемой функции
-		var t = this
-		func.__IEwrapper = func.__IEwrapper || function (e)
+		var key = '__IEEventWrapper:'// + type + ':' + dir + ':' + this.uniqueID
+		
+		// JavaScript — сила!
+		// Каково: сохранить функцию-обертку в свойстве оборачиваемой функции
+		if (func[key])
 		{
-			e.target = e.srcElement
-			e.preventDefault  = function () { var old = this.returnValue;  this.returnValue = false; return old }
-			e.stopPropagation = function () { var old = this.cancelBubble; this.cancelBubble = true; return old }
-			e.detail = - e.wheelDelta / 30
-			func.apply(t, [e])
+			this.detachEvent('on' + type, func[key])
+		}
+		else
+		{
+			var t = this
+			func[key] = function (e)
+			{
+				e.target = e.srcElement
+				e.preventDefault  = Programica.IEFixes.preventDefault
+				e.stopPropagation = Programica.IEFixes.stopPropagation
+				e.detail = - e.wheelDelta / 30
+				func.apply(t, [e])
+			}
 		}
 		
-		this.detachEvent('on' + type, func.__IEwrapper)
-		this.attachEvent('on' + type, func.__IEwrapper)
-		
+		this.attachEvent('on' + type, func[key])
 	}
+	
 	self.addEventListener = document.addEventListener = Element.prototype.addEventListener
 }
 
-// кривоватый фикс removeEventListener(...)
+// quick fix for removeEventListener(...)
 if (!self.removeEventListener && self.detachEvent)
 {
 	Element.prototype.removeEventListener = function (type, func, dir)
@@ -193,8 +192,11 @@ if (!self.removeEventListener && self.detachEvent)
 		if (Programica.IEFixes.eventConversion[type])
 			type = Programica.IEFixes.eventConversion[type]
 		
-		this.detachEvent('on' + type, func.__IEwrapper)
+		var key = '__IEEventWrapper:'// + type + ':' + dir + ':' + this.uniqueID
+		if (func[key])
+			this.detachEvent('on' + type, func[key])
 	}
+	
 	self.removeEventListener = document.removeEventListener = Element.prototype.removeEventListener
 }
 
@@ -245,7 +247,7 @@ if (!self.removeEventListener && self.detachEvent)
 	)
 }
 
-// опять криво имитируем getAttributeNS(...)
+// another dirty fix: now for getAttributeNS(...)
 if (!document.getAttributeNS)
 	Element.prototype.getAttributeNS = function (ns, attr) { return this.getAttribute(Programica.XMLNS[ns] + ':' + attr) }
 
@@ -272,17 +274,17 @@ function $E  (type, props)
 }
 
 
-//document.write('<script id="__ie_onload" defer="defer" src="javascript:void(0)"></script>')
-//document.getElementById("__ie_onload").onreadystatechange = function ()
-//{
-//	if (this.readyState == "complete")
-//		Programica.IEFixes.fixThem(document.all)
-//}
-
 self.addEventListener('load', function () { Programica.IEFixes.fixThem(document.all) })
 
-document.write('<style> html * { scrollbar-face-color: expression((runtimeStyle.scrollbarFaceColor = 0), fixIE(this)) } </style>')
-//document.write('<style> * { behavior: expression(fixIE(this)) } </style>')
+Programica.IEFixes.CSSBindings =
+[
+'label{scrollbar-base-color:expression((runtimeStyle.scrollbarBaseColor="transparent"),Programica.IEFixes.fixLabel(this))}',
+'img{scrollbar-highlight-color:expression((runtimeStyle.scrollbarHighlightColor="transparent"),(title||(title="")))}'//,
+//'*{scrollbar-face-color:expression((runtimeStyle.scrollbarFaceColor="transparent"),fixIE(this))}'
+]
+
+document.write('<style>' + Programica.IEFixes.CSSBindings.join('\n') + '</style>')
+
 
 
 // for oldstyle popups
@@ -290,8 +292,3 @@ if (self.dialogArguments && self.dialogArguments.external)
 	self.extern = self.dialogArguments.external
 else if (self.external)
 	self.extern = self.external
-
-//var catcher = document.createElement('div')
-//catcher.onpropertychange = function () { alert(event.propertyName + ' = ' + this[event.propertyName]) }
-//$$$('head')[0].appendChild(node)
-////node.x = undefined
