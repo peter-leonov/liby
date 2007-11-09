0a15bb80019524d327cdca551583fb19c379d5f2
Peter Leonov <peter@kung-fu-tzu.ru>
2007-11-09 16:40:23 +0000

    ~ Programica -> with(P:Programica){ P … P … P }

--- htdocs/lib/Programica/IEFixes.js
+++ htdocs/lib/Programica/IEFixes.js
@@ -3,11 +3,19 @@ if (!self.Programica) Programica = {}
 if (!self.HTMLFormElement) self.HTMLFormElement = {prototype:{}}
 if (!self.Element) self.Element = {prototype:{}}
 
-with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(.+?)"\)$/})
-{
-	Programica.IEFixes =
+with
+(
 	{
-		fixOpacity: function (node)
+		P: Programica,
+		e: self.Element.prototype,
+		hfe: HTMLFormElement.prototype,
+		rex1: /url\("(.+?)"\)$/
+	}
+)
+{
+	P.IEFixes =
+	{
+		opacity: function (node)
 		{
 			if ((s = node.style))
 			{
@@ -20,21 +28,22 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(
 			}
 		},
 		
-		fixDisabled: function (node)
+		disabled: function (node)
 		{
 			node.addClassName('disabled')
 			node.disabled ? node.addClassName('disabled') : node.remClassName('disabled')
 		},
 		
 		// отчасти исправляет обработку png
-		fixImgPng: function (node)
+		imgPng: function (node)
 		{
 
 			node.runtimeStyle.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + node.src + "', sizingMethod='image')"
 			node.setAttribute('src', '/lib/img/dot.gif')
 			//node.runtimeStyle.visibility = 'hidden'
 		},
-		fixBgPng: function (node)
+		
+		bgPng: function (node)
 		{
 			// rex1 is static, so I cut it out to the with(...)
 			var m = rex1.exec(node.currentStyle.backgroundImage)
@@ -43,7 +52,7 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(
 		},
 		
 		// делает кликабельными метки
-		fixLabel: function (node)
+		label: function (node)
 		{
 			node.attachEvent
 			(
@@ -54,15 +63,9 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(
 					// может элемент принять фокус или нет
 					try
 					{
-						if
-						(
-							n =
-							(
-								this.getElementsByTagName('input')[0] ||
+						if (n = (this.getElementsByTagName('input')[0] ||
 								this.getElementsByTagName('textarea')[0] ||
-								this.getElementsByTagName('select')[0]
-							)
-						)
+								this.getElementsByTagName('select')[0]))
 							n.click(), n.focus()
 					}
 					catch (ex) { log(ex.message) }
@@ -71,16 +74,17 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(
 		},
 		
 		// добавляет методы и свойства из Element.prototype
-		fixPrototype: function (node)
+		proto: function (node)
 		{
 			for (var p in e)
 				node[p] = e[p]
-			
-			if (node.tagName == 'FORM')
-			{
-				for (var p in hfe)
-					node[p] = hfe[p]
-			}
+		},
+		
+		// добавляет методы и свойства из HTMLFormElement.prototype
+		formProto: function (node)
+		{
+			for (var p in hfe)
+				node[p] = hfe[p]
 		},
 		
 		fixThem: function (nodes)
@@ -89,206 +93,207 @@ with ({e: self.Element.prototype, hfe: HTMLFormElement.prototype, rex1: /url\("(
 				fixIE(nodes[i])
 		}
 	}
-}
-
-with (Programica.IEFixes)
-{
-	var onpropertychange6 = function ()
-	{
-		if (event.propertyName == 'style.opacity')
-			fixOpacity(this)
-		else if (event.propertyName == 'disabled')
-			fixDisabled(this)
-		else if (event.propertyName == 'innerHTML')
-			fixThem(this.all)
-	}
 	
-	var onpropertychange7 = function ()
+	with (P.IEFixes)
 	{
-		if (event.propertyName == 'style.opacity')
-			fixOpacity(this)
-		else if (event.propertyName == 'innerHTML')
-			fixThem(this.all)
-	}
-	
-	fixIE6 = function (node)
-	{
-		fixPrototype(node)
-		node.onpropertychange = onpropertychange6
-	}
-	
-	var fixIE7 = function (node)
-	{
-		fixPrototype(node)
-		node.onpropertychange = onpropertychange7
-	}
-	
-	if (/MSIE 7/.test(navigator.userAgent))
-		fixIE = fixIE7
-	else if (/MSIE 6/.test(navigator.userAgent))
-		fixIE = fixIE6
-	
-	fixIEImgPng = fixImgPng
-	fixIEBgPng = fixBgPng
-}
-
-
-
-Programica.IEFixes.eventConversion = { DOMMouseScroll: 'mousewheel' }
-
-Programica.IEFixes.preventDefault = function ()
-{
-	var old = this.returnValue
-	this.returnValue = false
-	return old
-}
-
-Programica.IEFixes.stopPropagation = function ()
-{
-	var old = this.cancelBubble
-	this.cancelBubble = true
-	return old
-}
-
-// yet another addEventListener(...) fix
-if (!self.addEventListener && self.attachEvent)
-{
-	Element.prototype.addEventListener = function (type, func, dir)
-	{
-		type = Programica.IEFixes.eventConversion[type] || type
-		
-		var key = '__IEEventWrapper:'// + type + ':' + dir + ':' + this.uniqueID
-		
-		// JavaScript — сила!
-		// Каково: сохранить функцию-обертку в свойстве оборачиваемой функции
-		if (func[key])
+		var onpropertychange6 = function ()
 		{
-			this.detachEvent('on' + type, func[key])
+			if (event.propertyName == 'style.opacity')
+				opacity(this)
+			else if (event.propertyName == 'disabled')
+				disabled(this)
+			else if (event.propertyName == 'innerHTML')
+				fixThem(this.all)
 		}
-		else
+		
+		var onpropertychange7 = function ()
 		{
-			var t = this
-			func[key] = function (e)
+			if (event.propertyName == 'style.opacity')
+				opacity(this)
+			else if (event.propertyName == 'innerHTML')
+				fixThem(this.all)
+		}
+		
+		fixIE6 = function (node)
+		{
+			proto(node)
+			node.onpropertychange = onpropertychange6
+		}
+		
+		var fixIE7 = function (node)
+		{
+			proto(node)
+			node.onpropertychange = onpropertychange7
+		}
+		
+		if (/MSIE 7/.test(navigator.userAgent))
+			fixIE = fixIE7
+		else if (/MSIE 6/.test(navigator.userAgent))
+			fixIE = fixIE6
+		
+		fixIEImgPng = imgPng
+		fixIEBgPng = bgPng
+	}
+	
+	
+	
+	P.IEFixes.eventConversion = { DOMMouseScroll: 'mousewheel' }
+	
+	P.IEFixes.preventDefault = function ()
+	{
+		var old = this.returnValue
+		this.returnValue = false
+		return old
+	}
+	
+	P.IEFixes.stopPropagation = function ()
+	{
+		var old = this.cancelBubble
+		this.cancelBubble = true
+		return old
+	}
+	
+	// yet another addEventListener(...) fix
+	if (!self.addEventListener && self.attachEvent)
+	{
+		Element.prototype.addEventListener = function (type, func, dir)
+		{
+			type = P.IEFixes.eventConversion[type] || type
+			
+			var key = '__IEEventWrapper:'// + type + ':' + dir + ':' + this.uniqueID
+			
+			// JavaScript — сила!
+			// Каково: сохранить функцию-обертку в свойстве оборачиваемой функции
+			if (func[key])
 			{
-				e.target = e.srcElement
-				e.preventDefault  = Programica.IEFixes.preventDefault
-				e.stopPropagation = Programica.IEFixes.stopPropagation
-				e.detail = - e.wheelDelta / 30
-				func.apply(t, [e])
+				this.detachEvent('on' + type, func[key])
 			}
+			else
+			{
+				var t = this
+				func[key] = function (e)
+				{
+					e.target = e.srcElement
+					e.preventDefault  = P.IEFixes.preventDefault
+					e.stopPropagation = P.IEFixes.stopPropagation
+					e.detail = - e.wheelDelta / 30
+					func.apply(t, [e])
+				}
+			}
+			
+			this.attachEvent('on' + type, func[key])
 		}
 		
-		this.attachEvent('on' + type, func[key])
+		self.addEventListener = document.addEventListener = Element.prototype.addEventListener
 	}
 	
-	self.addEventListener = document.addEventListener = Element.prototype.addEventListener
-}
-
-// quick fix for removeEventListener(...)
-if (!self.removeEventListener && self.detachEvent)
-{
-	Element.prototype.removeEventListener = function (type, func, dir)
+	// quick fix for removeEventListener(...)
+	if (!self.removeEventListener && self.detachEvent)
 	{
-		if (Programica.IEFixes.eventConversion[type])
-			type = Programica.IEFixes.eventConversion[type]
-		
-		var key = '__IEEventWrapper:'// + type + ':' + dir + ':' + this.uniqueID
-		if (func[key])
-			this.detachEvent('on' + type, func[key])
-	}
-	
-	self.removeEventListener = document.removeEventListener = Element.prototype.removeEventListener
-}
-
-// sorry mom, i had to fix this ****** onload call order bug
-{
-	this['on-load-listeners'] = []
-	
-	self.addEventListenerReal = self.addEventListener
-	self.addEventListener = function (type, func, dir)
-	{
-		if (type == 'load')
+		Element.prototype.removeEventListener = function (type, func, dir)
 		{
-			this.removeEventListener(type, func, dir)
-			this['on-load-listeners'].push(func)
+			if (P.IEFixes.eventConversion[type])
+				type = P.IEFixes.eventConversion[type]
+			
+			var key = '__IEEventWrapper:'// + type + ':' + dir + ':' + this.uniqueID
+			if (func[key])
+				this.detachEvent('on' + type, func[key])
 		}
-		else
-			this.addEventListenerReal(type, func, dir)
 		
-		return func
+		self.removeEventListener = document.removeEventListener = Element.prototype.removeEventListener
 	}
 	
-	self.removeEventListenerReal = self.removeEventListener
-	self.removeEventListener = function (type, func, dir)
+	// sorry mom, i had to fix this ****** onload call order bug
 	{
-		if (type == 'load')
+		this['on-load-listeners'] = []
+		
+		self.addEventListenerReal = self.addEventListener
+		self.addEventListener = function (type, func, dir)
 		{
-			var newarr = []
-			for (var i = 0; i < this['on-load-listeners'].length; i++)
-				if (this['on-load-listeners'][i] != func)
-					newarr.push(this['on-load-listeners'][i])
+			if (type == 'load')
+			{
+				this.removeEventListener(type, func, dir)
+				this['on-load-listeners'].push(func)
+			}
+			else
+				this.addEventListenerReal(type, func, dir)
+			
+			return func
 		}
-		else
-			this.removeEventListenerReal(type, func, dir)
 		
-		return func
-	}
-	
-	// we have already done the wrapper job
-	self.addEventListenerReal
-	(
-		'load',
-		function (e)
+		self.removeEventListenerReal = self.removeEventListener
+		self.removeEventListener = function (type, func, dir)
 		{
-			for (var i = 0; i < this['on-load-listeners'].length; i++)
-				this['on-load-listeners'][i](e)
-		},
-		true
-	)
-}
-
-// another dirty fix: now for getAttributeNS(...)
-if (!document.getAttributeNS)
-	Element.prototype.getAttributeNS = function (ns, attr) { return this.getAttribute(Programica.XMLNS[ns] + ':' + attr) }
-
-document.realIECreateElement = document.createElement
-document.createElement = function (type)
-{
-	var e = document.realIECreateElement(type)
-	Programica.IEFixes.fixPrototype(e)
-	return e
-}
-
-function $E  (type, props)
-{
-	if (props)
-	{
-		var html = []
-		for (var i in props)
-			html.push(i + '="' + encodeURI(props[i]) + '"')
+			if (type == 'load')
+			{
+				var newarr = []
+				for (var i = 0; i < this['on-load-listeners'].length; i++)
+					if (this['on-load-listeners'][i] != func)
+						newarr.push(this['on-load-listeners'][i])
+			}
+			else
+				this.removeEventListenerReal(type, func, dir)
+			
+			return func
+		}
 		
-		return document.createElement('<' + type + ' ' + html.join(' ') + '>')
+		// we have already done the wrapper job
+		self.addEventListenerReal
+		(
+			'load',
+			function (e)
+			{
+				for (var i = 0; i < this['on-load-listeners'].length; i++)
+					this['on-load-listeners'][i](e)
+			},
+			true
+		)
 	}
 	
-	return document.createElement(type)
+	// another dirty fix: now for getAttributeNS(...)
+	if (!document.getAttributeNS)
+		Element.prototype.getAttributeNS = function (ns, attr) { return this.getAttribute(P.XMLNS[ns] + ':' + attr) }
+	
+	document.realIECreateElement = document.createElement
+	document.createElement = function (type)
+	{
+		var e = document.realIECreateElement(type)
+		P.IEFixes.proto(e)
+		return e
+	}
+	
+	function $E  (type, props)
+	{
+		if (props)
+		{
+			var html = []
+			for (var i in props)
+				html.push(i + '="' + encodeURI(props[i]) + '"')
+			
+			return document.createElement('<' + type + ' ' + html.join(' ') + '>')
+		}
+		
+		return document.createElement(type)
+	}
+	
+	
+	self.addEventListener('load', function () { P.IEFixes.fixThem(document.all) })
+	
+	P.IEFixes.CSSBindings =
+	[
+	'label{scrollbar-base-color:expression((runtimeStyle.scrollbarBaseColor="transparent"),Programica.IEFixes.label(this))}',
+	'img{scrollbar-highlight-color:expression((runtimeStyle.scrollbarHighlightColor="transparent"),(title||(title="")))}',
+	'form{scrollbar-face-color:expression((runtimeStyle.scrollbarFaceColor="transparent"),Programica.IEFixes.formProto(this))}'
+	//'*{scrollbar-face-color:expression((runtimeStyle.scrollbarFaceColor="transparent"),fixIE(this))}'
+	]
+	
+	document.write('<style>' + P.IEFixes.CSSBindings.join('\n') + '</style>')
+	
+	
+	
+	// for oldstyle popups
+	if (self.dialogArguments && self.dialogArguments.external)
+		self.extern = self.dialogArguments.external
+	else if (self.external)
+		self.extern = self.external
 }
-
-
-self.addEventListener('load', function () { Programica.IEFixes.fixThem(document.all) })
-
-Programica.IEFixes.CSSBindings =
-[
-'label{scrollbar-base-color:expression((runtimeStyle.scrollbarBaseColor="transparent"),Programica.IEFixes.fixLabel(this))}',
-'img{scrollbar-highlight-color:expression((runtimeStyle.scrollbarHighlightColor="transparent"),(title||(title="")))}'//,
-//'*{scrollbar-face-color:expression((runtimeStyle.scrollbarFaceColor="transparent"),fixIE(this))}'
-]
-
-document.write('<style>' + Programica.IEFixes.CSSBindings.join('\n') + '</style>')
-
-
-
-// for oldstyle popups
-if (self.dialogArguments && self.dialogArguments.external)
-	self.extern = self.dialogArguments.external
-else if (self.external)
-	self.extern = self.external
