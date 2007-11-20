c43baccf0c650b9dcf4cf528f0b3575ed502f5f8
Peter Leonov <peter@kung-fu-tzu.ru>
2007-11-20 19:27:19 +0000

    + Numper.plural String.plural

--- htdocs/lib/Programica/Init.js
+++ htdocs/lib/Programica/Init.js
@@ -74,6 +74,24 @@ function extend (to, from)
 if (!String.localeCompare)
 	String.localeCompare = function (a, b) { return a < b ? -1 : (a > b ? 1 : 0) }
 
+String.prototype.plural = Number.prototype.plural = function (a, b, c)
+{
+	if (this % 1)
+		return b
+	
+	var v = Math.abs(this) % 100
+	if (11 <= v && v <= 19)
+		return c
+	
+	v = v % 10
+	if (2 <= v && v <= 4)
+		return b
+	if (v == 1)
+		return a
+	
+	return c
+}
+
 function stringify (obj)
 {
 	var props = []
