ad74a13144f0311aff3673a1167b8e2f5706312f
Peter Leonov <peter@kung-fu-tzu.ru>
2008-01-11 10:44:10 +0000

    + new light BugsCatcher

--- htdocs/lib/Programica/BugsCatcher.js
+++ htdocs/lib/Programica/BugsCatcher.js
@@ -5,39 +5,18 @@ try { console.log('Programica BugsCatcher enabled') } catch (ex) {}
 
 //window.onerror = function (a,b,c) { alert(a) }
 
-var bugsCatcher =
-{
-	errStack: [],
-	
-	save: function (message, url, line)
-	{
-		try
-		{
-			var err = message + '\n' + url + ':' + line
-			bugsCatcher.errStack.push((window.encodeURIComponent || window.escape)(err))
-		}
-		catch (ex) { /*alert(ex.message)*/ }
-	
-		return true
-	},
-	
-	send: function ()
-	{
-		if (bugsCatcher.errStack.length)
-		{
-			var data = bugsCatcher.errStack.join(';')
-			var fr = document.createElement('img')
-			fr.src = '/lib/bugs-catcher.cgi?errs=' + data
-		
-			bugsCatcher.errStack = []
-		}
-	}
-}
-
 self.onerror = window.onerror = function (message, url, line)
 {
-	try { bugsCatcher.save(message, url, line) } catch (ex) { /*alert(ex.message)*/ }
+	try
+	{
+		var err = message + '\n' + url + ':' + line
+		
+		var fr = document.createElement('img')
+		fr.src = '/lib/bugs-catcher.cgi?' + (window.encodeURIComponent || window.escape)(err)
+ 	}
+	catch (ex) { alert(ex.message) }
+	
 	return true
 }
 
-timer = setInterval(function () { bugsCatcher.send() }, 3000)
+
