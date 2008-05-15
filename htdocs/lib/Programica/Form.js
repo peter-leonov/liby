c2c37b61c1522f8bc8df7a0cf63e5b83b5af1a9e
Peter Leonov <peter@kung-fu-tzu.ru>
2008-05-15 17:24:31 +0000

    + Forms.js: hash2form

--- htdocs/lib/Programica/Form.js
+++ htdocs/lib/Programica/Form.js
@@ -58,18 +58,114 @@ Programica.Form =
 		return hash;
 	},
 	
-	hash2form: function (h, f)
+	indexFormHash: function (h)
 	{
-		var node, val, nn, hash = {}
-		
+		var res = {}
+		for (var k in h)
+		{
+			var v = h[k]
+			var ndx = res[k] = {}
+			if (v === undefined)
+				ndx
+			if (v.constructor == Array)
+				for (var i = 0; i < v.length; i++)
+					ndx[v[i]] = ndx[v[i]] ? ndx[v[i]] + 1 : 1
+			else if (v.constructor == String)
+				ndx[v] = 1
+			else
+				throw new Error('Can`t proceed key of constructor "' + v.constructor + '"')
+		}
+		log(res)
+		return res
+	},
+	
+	hash2form1: function (h, f)
+	{
+		h = this.indexFormHash(h)
 		for (var i = 0; i < f.length; i++)
 		{
-			node = f.elements[i]
-			nn = node.name
+			var node = f.elements[i]
+			var nn = node.name
 			
-			log(nn)
+			if (h[nn])
+				switch (node.type)
+				{
+					case 'checkbox':
+					case 'radio':
+						if (h[nn][node.value])
+							h[nn][node.value]--, node.checked =  'checked'
+						break
+				
+					case 'select-one':
+						val = node.options[node.selectedIndex].value
+						break
+					
+					case 'submit':
+						break
+					
+					default:
+						for (var k in h[nn])
+							if (h[nn][k])
+							{
+								h[nn][k]--
+								node.value = k
+								break
+							}
+				}
 		}
-		
-		return hash;
+	},
+	
+	forceArrayFormHash: function (h)
+	{
+		var res = {}
+		for (var k in h)
+		{
+			var v = h[k]
+			if (typeof v === 'undefined')
+				throw new Error('Can`t proceed undefined value for key "' + k + '"')
+			else if (typeof v == 'string')
+				res[k] = [String(v)]
+			else if (v instanceof Array)
+				res[k] = v.slice()
+			else
+				throw new Error('Can`t proceed key of constructor "' + v.constructor + '"')
+		}
+		log(res)
+		return res
+	},
+	
+	hash2form: function (h, f)
+	{
+		h = this.forceArrayFormHash(h)
+		for (var i = 0; i < f.length; i++)
+		{
+			var node = f.elements[i]
+			var v
+			
+			if (v = h[node.name])
+				switch (node.type)
+				{
+					case 'checkbox':
+					case 'radio':
+						if (v[0] == node.value)
+							v.shift(), node.checked = 'checked'
+						break
+					
+					case 'select-one':
+						for (var j = 0; j < node.options.length; j++)
+							if (v[0] == node.options[j].value)
+							{
+								node.selectedIndex = j
+								v.shift()
+								break
+							}
+						break
+					
+					case 'text':
+					case 'textarea':
+						node.value = v.shift()
+						break
+				}
+		}
 	}
 }
