19fcd27ed9b824f3c2731fbfe0fb188603c84b5b
Peter Leonov <peter@kung-fu-tzu.ru>
2008-05-15 02:40:17 +0000

    ~ some garbage collection ;)

--- htdocs/lib/Programica/Form.js
+++ htdocs/lib/Programica/Form.js
@@ -1,12 +1,13 @@
 // example: $('form').toHash()
-HTMLFormElement.prototype.toHash = function (fa) { return Programica.Form.form2hash(this, fa) }
+HTMLFormElement.prototype.toHash = function (forse_array) { return Programica.Form.form2hash(this, forse_array) }
+HTMLFormElement.prototype.fromHash = function (hash) { return Programica.Form.hash2form(hash, this) }
 
 Programica.Form = {}
 
 // Преобразование данных формы в хеш
 Programica.Form =
 {
-	form2hash: function (f, fa)
+	form2hash: function (f, forse_array)
 	{
 		var node, val, nn, hash = {}
 		
@@ -44,18 +45,31 @@ Programica.Form =
 			if (val != null)
 			{
 				if	(hash[nn] == null)
-					hash[nn] = fa ? [val] : val
+					hash[nn] = forse_array ? [val] : val
 				else if (hash[nn].constructor == Array)
 					hash[nn].push(val)
 				else
 					hash[nn] = [hash[nn], val]
 			}
-			else if (fa && !hash[nn])
+			else if (forse_array && !hash[nn])
 				hash[nn] = []
+		}
+		
+		return hash;
+	},
+	
+	hash2form: function (h, f)
+	{
+		var node, val, nn, hash = {}
+		
+		for (var i = 0; i < f.length; i++)
+		{
+			node = f.elements[i]
+			nn = node.name
+			
+			log(nn)
 		}
 		
 		return hash;
 	}
 }
-
-log2("Programica/Form.js loaded")
