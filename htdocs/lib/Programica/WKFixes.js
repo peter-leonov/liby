925cb039e513c3c63c3828ee1b0e2dd900e9db48
Peter Leonov <peter@kung-fu-tzu.ru>
2008-06-04 15:00:38 +0000

    ~ WKFixes: some code enlightment

--- htdocs/lib/Programica/WKFixes.js
+++ htdocs/lib/Programica/WKFixes.js
@@ -17,26 +17,14 @@ window.addEventListener
 		e.stopPropagation()
 		
 		var d = (- e.wheelDelta / 120)
-		
 		var ne = document.createEvent('UIEvents')
-		
 		if (ne.initUIEvent)
 			ne.initUIEvent('DOMMouseScroll', true, true, ne.view, d)
 		else
 			ne.initEvent('DOMMouseScroll', true, true, ne.view, d)
 		
-		with (e)
-		{
-			ne.detail = d
-			ne.screenX = screenX
-			ne.screenY = screenY
-			ne.clientX = clientX
-			ne.clientY = clientY
-			ne.ctrlKey = ctrlKey
-			ne.altKey = altKey
-			ne.metaKey = metaKey
-			ne.shiftKey = shiftKey
-		}
+		Object.extend(ne, e)
+		ne.detail = d
 		
 		if (!e.target.dispatchEvent(ne))
 			e.preventDefault()
