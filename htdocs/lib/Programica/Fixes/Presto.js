2a0396cce48226959359635d1efbbbf70ba5acaf
Peter Leonov <peter@kung-fu-tzu.ru>
2008-06-20 12:25:21 +0000

    ~ Opare fixes useragent guessing: SSI -> JS

--- htdocs/lib/Programica/Fixes/Presto.js
+++ htdocs/lib/Programica/Fixes/Presto.js
@@ -3,29 +3,30 @@ if (self.opera && opera.postError)
 	self.log = function () { return opera.postError(arguments) }
 
 
-<!--#if expr="$HTTP_USER_AGENT = /Opera\/9\.2/" -->
-// Opera screen image caching fix
-window.addEventListener
-(
-	'load',
-	function (e)
-	{
-		var d = document.documentElement
-		setInterval
-		(
-			function ()
-			{
-				try
+if (/Opera\/9\.2/.test(navigator.userAgent))
+{
+	// Opera screen image caching fix
+	window.addEventListener
+	(
+		'load',
+		function (e)
+		{
+			var d = document.documentElement
+			setInterval
+			(
+				function ()
 				{
-					var old = d.getComputedStyle().backgroundRepeat
-					d.style.backgroundRepeat = 'none'
-					d.style.backgroundRepeat = old
-				}
-				catch (ex) {}
-			},
-			250
-		)
-	},
-	false
-)
-<!--#endif -->
+					try
+					{
+						var old = d.getComputedStyle().backgroundRepeat
+						d.style.backgroundRepeat = 'none'
+						d.style.backgroundRepeat = old
+					}
+					catch (ex) {}
+				},
+				250
+			)
+		},
+		false
+	)
+}
