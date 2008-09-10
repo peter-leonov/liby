d236e39792fdf81912aaf9175ed35dedc030179d
Peter Leonov <peter@kung-fu-tzu.ru>
2008-09-10 16:36:06 +0000

    + text size adjuster

--- htdocs/lib/Programica/Humanize.js
+++ htdocs/lib/Programica/Humanize.js
@@ -22,3 +22,28 @@ Date.rusMonths = ['Январь', 'Февраль', 'Март', 'Апрель', 
 Date.rusMonths2 = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
 Date.prototype.toRusDate = function () { return this.getDate() + ' ' + Date.rusMonths2[this.getMonth()] + ' ' + this.getFullYear() }
 
+Humanize =
+{
+	adjustTextSize: function (nodes)
+	{
+		for (var i = 0; i < nodes.length; i++)
+		{
+			var node = nodes[i]
+			
+			if (node.scrollWidth > node.offsetWidth)
+			{
+				var text = node.firstChild,
+					string = text.nodeValue
+				node.realText = string
+				text.nodeValue = string.substr(0, 16) + '…'
+				node.title = string
+			}
+		}
+	},
+	
+	adjustTextSizeOfNodes: function (root, selector)
+	{
+		var me = this
+		setTimeout(function () { me.adjustTextSize(cssQuery(selector, root)) }, 1)
+	}
+}
