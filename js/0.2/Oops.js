9b98e5a66b45a47bb3a0c7a8117dd3a3833e9a46
Peter Leonov <peter@kung-fu-tzu.ru>
2009-02-01 02:13:45 +0000

    ~ Oops.js without ssi

--- js/0.2/Oops.js
+++ js/0.2/Oops.js
@@ -1,24 +1,33 @@
-<!--# if expr="$HOST != /[A-Za-z\-]+\.[A-Za-z\-]+.programica.ru/" -->
-try { console.log('Programica Oops enabled') } catch (ex) {}
-
 // at this stage no fixes or wrappers are loaded from any lib
 // so you can see some dirty "cross browser" code here
 
-function Oops (message, url, line, type)
+if (!(/[A-Za-z\-]+\.[A-Za-z\-]+.programica.ru/.test(window.location.host)))
 {
-	try
-	{
-		var esc = self.encodeURIComponent || self.escape
-		var q = 'url=' + esc(url + ':' + line) + '&message=' + esc(message) + '&session=' + Oops.randomId + '&type=' + (type || 'error')
-		document.createElement('img').src = 'http://oops.programica.ru/report?' + q
- 	}
-	catch (ex) {  }
+	try { console.log('Oops enabled') } catch (ex) {}
 	
-	return true
+	function Oops (message, url, line, type)
+	{
+		try // to fully describe an error
+		{
+			var href = 'http://oops.programica.ru/report',
+				esc = window.encodeURIComponent || window.escape,
+				q = 'url=' + esc(url + ':' + line) + '&message=' + esc(message) + '&session=' + Oops.session + '&type=' + (type || 'error')
+			
+			document.createElement('img').src = '?' + q
+		}
+		catch (ex)
+		{
+			try // to scream for help
+			{
+				document.createElement('img').src = href + '?message=cant+report&type=error'
+			}
+			catch (ex) {}
+		}
+		
+		return true
+	}
+	
+	Oops.session = (new Date()).getTime().toString() + Math.round(Math.random() * 1E+17)
+	
+	window.onerror = Oops
 }
-
-Oops.randomId = (new Date()).getTime().toString() + Math.round(Math.random() * 1E+17)
-
-self.reportError = function () { Oops(Array.prototype.slice.call(arguments).join(', '), document.location.href, -1, 'reportError') }
-self.onerror = Oops
-<!--# endif -->
