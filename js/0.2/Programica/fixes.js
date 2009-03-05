cd2ba498849bc2cfb47c58956c60520442581510
Peter Leonov <peter@kung-fu-tzu.ru>
2009-03-05 22:59:57 +0000

    ! filename case fix

--- js/0.2/Programica/fixes.js
+++ js/0.2/Programica/fixes.js
@@ -1,4 +1,4 @@
-<!--#if expr="$HTTP_USER_AGENT = /Gecko\//" --><!--#include file="Fixes/Gecko.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /Opera\//" --><!--#include file="Fixes/Presto.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /MSIE/" --><!--#include file="Fixes/Trident.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /WebKit\//" --><!--#include file="Fixes/WebKit.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /Gecko\//" --><!--#include file="fixes/Gecko.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /Opera\//" --><!--#include file="fixes/Presto.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /MSIE/" --><!--#include file="fixes/Trident.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /WebKit\//" --><!--#include file="fixes/WebKit.js" --><!--#endif -->
