bb8109f1129f33e46c36e865e6d76b54f14ab526
Peter Leonov <peter@kung-fu-tzu.ru>
2009-03-20 14:24:33 +0000

    ! IE 8 addEventListener fix (ported from v0.2)

--- js/0.1/Programica/Fixes.js
+++ js/0.1/Programica/Fixes.js
@@ -1,4 +1,5 @@
 <!--#if expr="$HTTP_USER_AGENT = /Gecko\//" --><!--#include file="Fixes/Gecko.js" --><!--#endif -->
 <!--#if expr="$HTTP_USER_AGENT = /Opera\//" --><!--#include file="Fixes/Presto.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /MSIE/" --><!--#include file="Fixes/Trident.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /MSIE [67]/" --><!--#include file="Fixes/Trident.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /MSIE 8/" --><!--#include file="Fixes/Trident8.js" --><!--#endif -->
 <!--#if expr="$HTTP_USER_AGENT = /WebKit\//" --><!--#include file="Fixes/WebKit.js" --><!--#endif -->
---
+++ js/0.1/Programica/Fixes/Trident8.js
@@ -0,0 +1,117 @@
+;(function(){
+
+var doc = document, win = window, undef
+
+if (!win.log)
+	win.log = function () {}
+
+if (!win.reportError)
+	win.reportError = win.log
+
+function onBeforeUnload ()
+{
+	var stack = onBeforeUnload.stack
+	for (var i = 0, il = stack.length; i < il; i++)
+	{
+		var el = stack[i]
+		el[0].detachEvent(el[1], el[2])
+	}
+	onBeforeUnload.stack = []
+}
+onBeforeUnload.stack = []
+
+
+var eventConversion = { DOMMouseScroll: 'mousewheel', unload: 'beforeunload' }
+
+function preventDefault ()
+{
+	var old = this.returnValue
+	this.returnValue = false
+	return old
+}
+
+function stopPropagation ()
+{
+	var old = this.cancelBubble
+	this.cancelBubble = true
+	return old
+}
+
+
+function getEventWrapper (node, type, func, dir)
+{
+	var pmc_uid = 'pmc::uniqueID'
+	node[pmc_uid] = node[pmc_uid] || node.uniqueID
+	var key = '__IEEventWrapper:' + type + ':' + dir + ':' + node[pmc_uid]
+	
+	if (func[key])
+		return func[key]
+	else
+	{
+		var wrapper = func[key] = function (e)
+		{
+			if (e === undef)
+				e = event
+			e.currentTarget = e.target = e.srcElement
+			e.preventDefault  = preventDefault
+			e.stopPropagation = stopPropagation
+			e.detail = - e.wheelDelta / 30
+			e.pageX = e.clientX + doc.documentElement.scrollLeft - doc.body.clientLeft // doc.body.scrollLeft
+			e.pageY = e.clientY + doc.documentElement.scrollTop  - doc.body.clientTop // doc.body.scrollTop
+			func.call(node, e)
+		}
+		wrapper.isIEEventWrapper = true
+		return wrapper
+	}
+}
+
+
+// yet another addEventListener(...) fix
+if (!win.addEventListener && win.attachEvent)
+	win.addEventListener = doc.addEventListener = Element.prototype.addEventListener = function (type, func, dir)
+	{
+		dir = dir || false
+		// window.status++
+		type = eventConversion[type] || type
+		
+		var wrapper = getEventWrapper(this, type, func, dir)
+		
+		if (wrapper)
+			this.detachEvent('on' + type, wrapper)
+		
+		if (type == 'change' && this.tagName == 'FORM')
+		{
+			if (!this.onchange)
+				this.onchange = function (e)
+				{
+					for (var i = 0; i < this.onchange.stack.length; i++)
+						this.onchange.stack[i].call(this, e || window.event)
+				}
+			
+			if (!this.onchange.stack)
+				this.onchange.stack = []
+			
+			this.onchange.stack.push(wrapper)
+		}
+		else
+		{
+			this.attachEvent('on' + type, wrapper)
+			onBeforeUnload.stack.push([this, 'on' + type, wrapper])
+		}
+	}
+
+// quick fix for removeEventListener(...)
+if (!win.removeEventListener && win.detachEvent)
+	win.removeEventListener = doc.removeEventListener = Element.prototype.removeEventListener = function (type, func, dir)
+	{
+		dir = dir || false
+		if (eventConversion[type])
+			type = eventConversion[type]
+		
+		var wrapper = getEventWrapper(this, type, func, dir)
+		if (wrapper)
+			this.detachEvent('on' + type, wrapper)
+	}
+
+
+})();
