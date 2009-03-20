97da543e75dec98a0a40ea6377012c13d36308f7
Peter Leonov <peter@kung-fu-tzu.ru>
2009-03-20 13:51:13 +0000

    ! IE 8 addEventListener fix (just the same as IE 6-7 fix)

--- js/0.2/Programica/fixes.js
+++ js/0.2/Programica/fixes.js
@@ -1,4 +1,7 @@
 <!--#if expr="$HTTP_USER_AGENT = /Gecko\//" --><!--#include file="fixes/Gecko.js" --><!--#endif -->
 <!--#if expr="$HTTP_USER_AGENT = /Opera\//" --><!--#include file="fixes/Presto.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /MSIE/" --><!--#include file="fixes/Trident.js" --><!--#endif -->
 <!--#if expr="$HTTP_USER_AGENT = /WebKit\//" --><!--#include file="fixes/WebKit.js" --><!--#endif -->
+
+<!--#if expr="$HTTP_USER_AGENT = /MSIE 6/" --><!--# include file="fixes/Trident.XHR.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /MSIE [67]/" --><!--#include file="fixes/Trident.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /MSIE 8/" --><!--#include file="fixes/Trident8.js" --><!--#endif -->
