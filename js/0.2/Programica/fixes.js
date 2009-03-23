582af8ba1d0dc4998ce7071b0b63fb7b39436bc0
Peter Leonov <peter@kung-fu-tzu.ru>
2009-03-23 21:03:13 +0000

    ! horrible IE 7 regression fix (XHR)

--- js/0.2/Programica/fixes.js
+++ js/0.2/Programica/fixes.js
@@ -2,6 +2,8 @@
 <!--#if expr="$HTTP_USER_AGENT = /Opera\//" --><!--#include file="fixes/Presto.js" --><!--#endif -->
 <!--#if expr="$HTTP_USER_AGENT = /WebKit\//" --><!--#include file="fixes/WebKit.js" --><!--#endif -->
 
-<!--#if expr="$HTTP_USER_AGENT = /MSIE 6/" --><!--# include file="fixes/Trident.XHR.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /MSIE [67]/" --><!--#include file="fixes/Trident.js" --><!--#endif -->
+<!--#if expr="$HTTP_USER_AGENT = /MSIE [67]/" -->
+<!--#include file="fixes/Trident.XHR.js" -->
+<!--#include file="fixes/Trident.js" -->
+<!--#endif -->
 <!--#if expr="$HTTP_USER_AGENT = /MSIE 8/" --><!--#include file="fixes/Trident8.js" --><!--#endif -->
