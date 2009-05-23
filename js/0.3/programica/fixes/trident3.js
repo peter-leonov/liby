46c978b07515dd6ae95c00a2f9f52751598af262
Peter Leonov <peter@kung-fu-tzu.ru>
2009-05-24 03:54:49 +0400

    more smart NodeShortcut fix

--- js/0.3/programica/fixes/trident3.js
+++ js/0.3/programica/fixes/trident3.js
@@ -14,38 +14,6 @@ if (!win.reportError)
 // so this is a fix for our Array.copy :)
 Array.copy = function (s) { var d = []; if (s !== undef) for (var i = 0, len = s.length; i < len; i++) d[i] = s[i]; return d }
 
-if (win.NodesShortcut)
-	win.NodesShortcut.E = function (tag, cn, props)
-	{
-		var node
-		if (props)
-		{
-			var html = [], name, i, names = ['name', 'type']
-			
-			for (i = 0; i < names.length; i++)
-			{
-				name = names[i]
-				if (name in props)
-				{
-					html.push(name + '="' + props[name].replace(/"/, '\\"') + '"')
-					delete props[name]
-				}
-			}
-			
-			node = doc.createElement('<' + tag + ' ' + html.join(' ') + '>')
-			
-			for (i in props)
-				node[i] = props[i]
-		}
-		else
-			node = doc.createElement(tag)
-		
-		if (cn !== undef) node.className = cn
-		
-		return node
-	}
-
-
 // appendChild, insertBefore, replaceChild
 
 if (!win.HTMLFormElement) win.HTMLFormElement = {prototype:{}}
@@ -330,11 +298,46 @@ function IEFixes ()
 	
 	win.addEventListener('load', fixDocumentAll)
 	
-	// for oldstyle popups
-	if (win.dialogArguments && win.dialogArguments.external)
-		win.extern = win.dialogArguments.external
-	else if (win.external)
-		win.extern = win.external
+	function fixNodesShortcut ()
+	{
+		if (win.NodesShortcut)
+		{
+			log('NodesShortcut.E was altered with fixed version')
+			win.NodesShortcut.E = function (tag, cn, props)
+			{
+				var node
+				if (props)
+				{
+					var html = [], name, i, names = ['name', 'type']
+					
+					for (i = 0; i < names.length; i++)
+					{
+						name = names[i]
+						if (name in props)
+						{
+							html.push(name + '="' + props[name].replace(/"/, '\\"') + '"')
+							delete props[name]
+						}
+					}
+					
+					node = doc.createElement('<' + tag + ' ' + html.join(' ') + '>')
+					
+					for (i in props)
+						node[i] = props[i]
+				}
+				else
+					node = doc.createElement(tag)
+				
+				if (cn !== undef)
+					node.className = cn
+				
+				return node
+			}
+		}
+	}
+	
+	win.addEventListener('load', fixDocumentAll)
+	win.addEventListener('load', fixNodesShortcut)
 }
 
 IEFixes()
