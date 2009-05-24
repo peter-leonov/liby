7385b49efdc1ccd5b0628b0b078292d348564888
Peter Leonov <peter@kung-fu-tzu.ru>
2009-05-24 04:53:21 +0400

    the humanize splat to the plural and the rus-date

---
+++ js/0.3/modules/rus-date.js
@@ -0,0 +1,9 @@
+;(function(){
+
+var D = Date,
+	rusMonths = D.rusMonths = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'],
+	rusMonths2 = D.rusMonths2 = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+
+D.prototype.toRusDate = function () { return this.getDate() + ' ' + rusMonths2[this.getMonth()] + ' ' + this.getFullYear() }
+
+})();
