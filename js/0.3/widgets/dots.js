bd458346525122940484b246e2efbc50233175c1
Peter Leonov <peter@kung-fu-tzu.ru>
2009-05-24 04:58:06 +0400

    v0.1 Humanize converted to the v0.3 dots :)

---
+++ js/0.3/widgets/dots.js
@@ -0,0 +1,28 @@
+;(function(){
+
+var myName = 'Dots',doc = document, undef
+
+Me = self[myName] =
+{
+	adjust: function (nodes, length)
+	{
+		if (length === undef)
+			length = 16
+		
+		for (var i = 0; i < nodes.length; i++)
+		{
+			var node = nodes[i]
+			
+			if (node.scrollWidth > node.offsetWidth)
+			{
+				var text = node.firstChild,
+					string = text.nodeValue
+				node.realText = string
+				text.nodeValue = string.substr(0, length) + '…'
+				node.title = string
+			}
+		}
+	}
+}
+
+})();
