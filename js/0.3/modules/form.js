485bcd23f8405f45ba522266bab36adc6781e705
Peter Leonov <peter@kung-fu-tzu.ru>
2009-06-01 02:53:49 +0400

    the form2hash() rewrite with an aggressive cleanup :)

--- js/0.3/modules/form.js
+++ js/0.3/modules/form.js
@@ -1,115 +1,67 @@
-// example: $('form').toHash()
-HTMLFormElement.prototype.toHash = function (forse_array) { return Programica.Form.form2hash(this, forse_array) }
-HTMLFormElement.prototype.fromHash = function (hash) { return Programica.Form.hash2form(hash, this) }
+;(function(){
 
-Programica.Form = {}
-
-// Преобразование данных формы в хеш
-Programica.Form =
+var Me = Programica.Form =
 {
-	form2hash: function (f, forse_array)
+	form2hash: function (form, fa)
 	{
-		var node, val, nn, hash = {}
+		var hash = {}, isArray = {}, els = form.elements
 		
-		for (var i = 0; i < f.length; i++)
+		for (var i = 0, l = els.length; i < l; i++)
 		{
-			node = f.elements[i]
-			nn = node.name
+			var node = els[i], name = node.name, val
 			
-			// skip nodes with exect empty names
-			if (nn === '')
+			// skip nodes with the exact empty names
+			if (name === '')
 				continue
 			
-			val = null
 			switch (node.type)
 			{
 				case 'checkbox':
-					val = node.checked ? node.value : null
-					break
-				
 				case 'radio':
-					val = node.checked ? node.value : null
+					if (node.checked)
+						val = node.value
+					else
+						continue
 					break
 				
 				case 'select-one':
 					val = node.options[node.selectedIndex].value
 					break
 				
-				case 'submit':
-					break
-				
 				default:
 					val = node.value
 			}
 			
-			if (val != null)
+			if (fa)
 			{
-				if	(hash[nn] == null)
-					hash[nn] = forse_array ? [val] : val
-				else if (hash[nn].constructor == Array)
-					hash[nn].push(val)
+				if (name in hash)
+					hash[name].push(val)
 				else
-					hash[nn] = [hash[nn], val]
+					hash[name] = [val]
 			}
-			else if (forse_array && !hash[nn])
-				hash[nn] = []
+			else
+			{
+				if (name in hash)
+				{
+					var v = hash[name]
+					if (isArray[name])
+						v.push(val)
+					else
+					{
+						isArray[name] = true
+						hash[name] = [v, val]
+					}
+				}
+				else
+					hash[name] = val
+			}
 		}
 		
 		return hash;
-	},
-	
-	forceArrayFormHash: function (h)
-	{
-		var res = {}
-		for (var k in h)
-		{
-			var v = h[k]
-			if (typeof v === 'undefined')
-				throw new Error('Can`t proceed undefined value for key "' + k + '"')
-			else if (typeof v == 'string')
-				res[k] = [String(v)]
-			else if (v instanceof Array)
-				res[k] = v.slice()
-			else
-				throw new Error('Can`t proceed key of constructor "' + v.constructor + '"')
-		}
-		return res
-	},
-	
-	hash2form: function (h, f, forse_array)
-	{
-		if (!forse_array)
-			h = this.forceArrayFormHash(h)
-		
-		for (var i = 0; i < f.length; i++)
-		{
-			var node = f.elements[i]
-			var v
-			
-			if (v = h[node.name])
-				switch (node.type)
-				{
-					case 'checkbox':
-					case 'radio':
-						if (v[0] == node.value)
-							v.shift(), node.checked = 'checked'
-						break
-					
-					case 'select-one':
-						for (var j = 0; j < node.options.length; j++)
-							if (v[0] == node.options[j].value)
-							{
-								node.selectedIndex = j
-								v.shift()
-								break
-							}
-						break
-					
-					case 'text':
-					case 'textarea':
-						node.value = v.shift()
-						break
-				}
-		}
 	}
 }
+
+// example: $('form').toHash()
+HTMLFormElement.prototype.toHash = function (fa) { return Me.form2hash(this, fa) }
+
+})();
