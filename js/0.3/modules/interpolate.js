340f04acc2b7c5aa9523229603ccff7fb802016d
Peter Leonov <peter@kung-fu-tzu.ru>
2009-06-14 04:52:19 +0400

    String#interpolate() major rewrite (testing is everything)

--- js/0.3/modules/interpolate.js
+++ js/0.3/modules/interpolate.js
@@ -1,4 +1,3 @@
-
 ;(function(){
 
 // fast and easy but looks terrible
@@ -84,64 +83,66 @@ function parseOutJS (src)
 }
 
 // bake() is used to increase compression level by placing nomungeble variables in one closure
-function bake ($_$h, $_$s, $_$o)
+function bake ($_$h, $_$s, $_$code)
 {
-	"$_$h:nomunge, $_$s:nomunge, $_$o:nomunge"
-	return eval('(0, function($_$h){with($_$h){return ' + $_$o.join('+') + '}})')
+	"$_$h:nomunge, $_$s:nomunge, $_$code:nomunge"
+	return eval('(0, function($_$h){with($_$h){return ' + $_$code + '}})')
+}
+
+function compile (str, hash)
+{
+	var b = parseOutJS(str), bl = b.length
+	
+	// string has no js
+	if (bl === 1)
+		return b.join('')
+	// string needs evaluation
+	else
+	{
+		var s = [], o = []
+		
+		s[0] = b[0]
+		o[0] = '($_$s[0])'
+		
+		for (var i = 1; i < bl; i+=2)
+		{
+			s.push(b[i+1])
+			
+			o.push('(' + b[i] + ')')
+			o.push('$_$s[' + (i + 1)/2 + ']')
+		}
+		
+		// if (!b[b.length-1])
+		// 	o.length--
+		try { return bake(hash, s, o.join('+')) }
+		catch (ex)
+		{
+			log('Error while compiling string: "' + str + '"')
+			throw ex
+		}
+	}
 }
 
 var cache = {}
 function interpolate (h)
 {
-	var i, b, s, o, f = cache[this]
-	if (!f)
+	var f = this in cache ? cache[this] : (cache[this] = compile(this))
+	
+	if (typeof f === 'string')
+		return f
+	else
 	{
-		b = parseOutJS(this), s = [], o = []
-		
-		s[0] = b[0]
-		o[0] = '($_$s[0])'
-		
-		for (i = 1; i < b.length; i+=2)
-		{
-			s.push(b[i+1])
-		
-			o.push('(' + b[i] + ')')
-			o.push('$_$s[' + (i + 1)/2 + ']')
-		}
-		
-		if (!b[b.length-1])
-			o.length--
-		try { f = cache[this] = bake(h, s, o) }
+		try { return f.apply(this, arguments) }
 		catch (ex)
 		{
-			log('Error while compiling string: "' + this + '"')
+			log('Error while executing string: "' + this + '"')
 			throw ex
 		}
-	}
-	try { return f.apply(this, arguments) }
-	catch (ex)
-	{
-		log('Error while executing string: "' + this + '"')
-		return null
 	}
 }
 
 interpolate.cache = cache
 String.prototype.interpolate = interpolate
 String.parseOutJS = parseOutJS
-
-// log('1${a}2${b}3${c}4'.interpolate({a:'A',b:'B',c:'C'}) === '1A2B3C4')
-// log('${a/*{"a{\\\'a}\\"}a"b}c*/} wor$ld ${n/*{{{"\'"}}}n\'n\'*/}'.interpolate({a:111,n:222}) === '111 wor$ld 222')
-// log('${a} text ${a} ${a} text ${b} ${b}'.interpolate({a:2222, b: 333}) === '2222 text 2222 2222 text 333 333')
-// log('${a} text ${a} ${a} text ${b} ${b} text'.interpolate({a:2222, b: 333}) === '2222 text 2222 2222 text 333 333 text')
-
-// // test for parseOutJS
-// var blocks = parseOutJS('"hello" \\${x} ${a{"a{\\\'a}\\"}a"b}c} wor$ld ${n{{{"\'"}}}n\'n\'}')
-// log(blocks[0] === '"hello" ${x} ')
-// log(blocks[1] === 'a{"a{\'a}"}a"b}c')
-// log(blocks[2] === ' wor$ld ')
-// log(blocks[3] === 'n{{{"\'"}}}n\'n\'')
-// log(blocks[4] === '')
-// log(blocks[5] === undefined)
 
 })();
