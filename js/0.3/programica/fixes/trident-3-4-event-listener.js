6e6c4ec0e48434e1df9c4e9218603a5a14a31338
Peter Leonov <peter@kung-fu-tzu.ru>
2009-06-21 18:38:31 +0400

    the list of supported event types added

--- js/0.3/programica/fixes/trident-3-4-event-listener.js
+++ js/0.3/programica/fixes/trident-3-4-event-listener.js
@@ -18,7 +18,8 @@ function onBeforeUnload ()
 onBeforeUnload.stack = []
 window.attachEvent('onbeforeunload', onBeforeUnload)
 
-var eventConversion = { DOMMouseScroll: 'mousewheel', unload: 'beforeunload' }
+var eventConversion = { DOMMouseScroll: 'mousewheel', unload: 'beforeunload' },
+	supportedEvents = {abort: 1, activate: 1, afterprint: 1, afterupdate: 1, beforeactivate: 1, beforecopy: 1, beforecut: 1, beforedeactivate: 1, beforeeditfocus: 1, beforepaste: 1, beforeprint: 1, beforeunload: 1, beforeupdate: 1, blur: 1, bounce: 1, e: 1, cellchange: 1, change: 1, click: 1, contextmenu: 1, controlselect: 1, copy: 1, cut: 1, dataavailable: 1, datasetchanged: 1, datasetcomplete: 1, dblclick: 1, deactivate: 1, drag: 1, dragend: 1, dragenter: 1, dragleave: 1, dragover: 1, dragstart: 1, drop: 1, error: 1, error: 1, errorupdate: 1, filterchange: 1, finish: 1, focus: 1, focusin: 1, focusout: 1, hashchange: 1, help: 1, keydown: 1, keypress: 1, keyup: 1, layoutcomplete: 1, load: 1, losecapture: 1, message: 1, mousedown: 1, mouseenter: 1, mouseleave: 1, mousemove: 1, mouseout: 1, mouseover: 1, mouseup: 1, mousewheel: 1, move: 1, moveend: 1, movestart: 1, offline: 1, online: 1, paste: 1, progress: 1, readystatechange: 1, reset: 1, resize: 1, resizeend: 1, resizestart: 1, rowenter: 1, rowexit: 1, rowsdelete: 1, rowsinserted: 1, scroll: 1, select: 1, selectionchange: 1, selectstart: 1, start: 1, stop: 1, storage: 1, storagecommit: 1, submit: 1, timeout: 1, unload: 1}
 
 function preventDefault ()
 {
@@ -52,7 +53,7 @@ function getEventWrapper (node, type, func, dir)
 			if (e === undef)
 				e = event
 			e.currentTarget = e.target = e.srcElement
-			e.preventDefault  = preventDefault
+			e.preventDefault = preventDefault
 			e.stopPropagation = stopPropagation
 			e.detail = - e.wheelDelta / 30
 			e.pageX = e.clientX + doc.documentElement.scrollLeft - doc.body.clientLeft // doc.body.scrollLeft
@@ -103,11 +104,13 @@ function initEvent (type, bubbles, cancelable)
 	this.bubbles = bubbles
 	this.cancelable = cancelable
 }
-doc.createEvent = function ()
+
+doc.createEvent = function (kind)
 {
-	var ne = doc.createEventObject()
-	ne.initEvent = initEvent
-	return ne
+	var e = doc.createEventObject()
+	e.initEvent = initEvent
+	e.timeStamp = +new Date()
+	return e
 }
 
 doc.dispatchEvent = Element.prototype.dispatchEvent = function (e)
