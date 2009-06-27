c506913eb64cbfee8b9b000e2620fe9e3587d283
Peter Leonov <peter@kung-fu-tzu.ru>
2009-06-28 02:19:14 +0400

    IE now supports all known event types :)

--- js/0.3/programica/fixes/trident-3-4-event-listener.js
+++ js/0.3/programica/fixes/trident-3-4-event-listener.js
@@ -21,10 +21,7 @@ window.attachEvent('onbeforeunload', onBeforeUnload)
 var eventTransport = doc.__pmc__eventTransport = 'beforeeditfocus',
 	supportedEvents = {abort:1, activate:1, afterprint:1, afterupdate:1, beforeactivate:1, beforecopy:1, beforecut:1, beforedeactivate:1, beforeeditfocus:1, beforepaste:1, beforeprint:1, beforeunload:1, beforeupdate:1, blur:1, bounce:1, cellchange:1, change:1, click:1, contextmenu:1, controlselect:1, copy:1, cut:1, dataavailable:1, datasetchanged:1, datasetcomplete:1, dblclick:1, deactivate:1, drag:1, dragend:1, dragenter:1, dragleave:1, dragover:1, dragstart:1, drop:1, error:1, errorupdate:1, filterchange:1, finish:1, focus:1, focusin:1, focusout:1, hashchange:1, help:1, keydown:1, keypress:1, keyup:1, layoutcomplete:1, load:1, losecapture:1, message:1, mousedown:1, mouseenter:1, mouseleave:1, mousemove:1, mouseout:1, mouseover:1, mouseup:1, mousewheel:1, move:1, moveend:1, movestart:1, offline:1, online:1, page:1, paste:1, progress:1, propertychange:1, readystatechange:1, reset:1, resize:1, resizeend:1, resizestart:1, rowenter:1, rowexit:1, rowsdelete:1, rowsinserted:1, scroll:1, select:1, selectionchange:1, selectstart:1, start:1, stop:1, storage:1, storagecommit:1, submit:1, timeout:1, unload:1}
 
-var Event = win.Event = function (event)
-{
-	this.constructor = Event
-}
+var Event = win.Event = function () { this.constructor = Event }
 
 Event.prototype =
 {
@@ -47,16 +44,29 @@ Event.prototype =
 	}
 }
 
+var DocumentEvent = win.DocumentEvent = function () { this.constructor = DocumentEvent }
+DocumentEvent.prototype = new Event()
+var UIEvent = win.UIEvent = function () { this.constructor = UIEvent }
+UIEvent.prototype = new Event()
+var MouseEvent = win.MouseEvent = function () { this.constructor = MouseEvent }
+MouseEvent.prototype = new Event()
+var KeyEvent = win.KeyEvent = function () { this.constructor = KeyEvent }
+KeyEvent.prototype = new Event()
+var MutationEvent = win.MutationEvent = function () { this.constructor = MutationEvent }
+MutationEvent.prototype = new Event()
+
+var eventConstructors = {Event:Event, DocumentEvent:DocumentEvent, UIEvent:UIEvent, MouseEvent:MouseEvent, KeyEvent:KeyEvent, MutationEvent:MutationEvent}
+
 
 window.uniqueID = document.uniqueID
 // uniqueID is not a constant for document
 document.__uniqueID = document.uniqueID
 
-function getEventWrapper (e)
+function getEventWrapper (e, kind)
 {
 	if (e.__pmc__wrapper)
 		return e.__pmc__wrapper
-	var w = new Event()
+	var w = new (eventConstructors[kind] || Event)()
 	
 	w.type = e.type
 	w.currentTarget = w.target = e.srcElement
@@ -125,7 +135,7 @@ win.removeEventListener = doc.removeEventListener = Element.prototype.removeEven
 
 doc.createEvent = function (kind)
 {
-	return getEventWrapper(doc.createEventObject())
+	return getEventWrapper(doc.createEventObject(), kind)
 }
 
 doc.dispatchEvent = Element.prototype.dispatchEvent = function (w)
