9cfca69d6bae76b7f09115d2fc6b9c0acf5ecf8e
Peter Leonov <peter@kung-fu-tzu.ru>
2009-06-28 02:12:31 +0400

    onload order fix moved to separate file

--- js/0.3/programica/fixes/trident3.js
+++ js/0.3/programica/fixes/trident3.js
@@ -2,5 +2,6 @@
 <!--# include file="trident-3-4-prototype.js" -->
 <!--# include file="trident-3-default-view.js" -->
 <!--# include file="trident-3-4-event-listener.js" -->
+<!--# include file="trident-3-4-event-order-onload.js" -->
 <!--# include file="trident-3-nodes-shortcut.js" -->
 <!--# include file="trident3-element-prototype.js" -->
--- js/0.3/programica/fixes/trident4.js
+++ js/0.3/programica/fixes/trident4.js
@@ -1,2 +1,3 @@
 <!--# include file="trident-3-4-prototype.js" -->
 <!--# include file="trident-3-4-event-listener.js" -->
+<!--# include file="trident-3-4-event-order-onload.js" -->
