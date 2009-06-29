d09f08214459019943b846db4f4af02f66cf5340
Peter Leonov <peter@kung-fu-tzu.ru>
2009-06-29 04:38:40 +0400

    simplified rewrite based on native Event object

--- js/0.3/modules/event-target.js
+++ js/0.3/modules/event-target.js
@@ -1,55 +1,9 @@
 ;(function(){
 
 var myName = 'EventTarget',
-	Me = self[myName] = {},
-	MyEvent = Me.Event = Class(myName + '.Event'),
-	handlersProp = '__' + myName + 'Handlers'
-
-MyEvent.prototype = 
-{
-	eventPhase: 2,
-	bubbles: false,
-	cancelable: true,
-	
-	initialize: function (target)
-	{
-		this.timeStamp = +new Date()
-		this.currentTarget = this.target = target
-	},
-	
-	stopPropagation: function () {},
-	preventDefault: function () { this._preventedDefault = true },
-	initEvent: function () {}
-}
-
-// var cache = Me.cache = {}
-
-function uselessDispatchEvent () { return true }
-
-function usefullDispatchEvent (e, data)
-{
-	var event = new MyEvent(this)
-	
-	if (typeof e == 'string')
-	{
-		event.type = e
-		event.data = data || {}
-	}
-	else
-		Object.extend(event, e)
-	
-	var handlers, harr, i, ret = true
-	if (handlers = this[handlersProp])
-		if (harr = handlers[event.type])
-			for (i = 0, len = harr.length; i < len; i++)
-				harr[i].call(this, event)
-	
-	return !event._preventedDefault
-}
-
-// event = document.createEvent("Event")
-// event.initEvent(type, bubbles, cancelable)
-// node.dispatchEvent(event)
+	Me = self[myName] = Module(myName),
+	handlersProp = '__' + myName + 'Handlers',
+	doc = document, undef
 
 Me.prototype =
 {
@@ -65,33 +19,45 @@ Me.prototype =
 		}
 		else
 			(this[handlersProp] = {})[type] = [listener]
-		
-		this.dispatchEvent = usefullDispatchEvent
 	},
-
+	
 	removeEventListener: function (type, listener, capture)
 	{
-		var handlers, harr, i, k, count
+		var handlers, harr, i
 		if (handlers = this[handlersProp])
-			if (harr = handlers[type])
+			if ((harr = handlers[type]))
 			{
 				if ((i = harr.indexOf(listener)) >= 0)
 				{
 					for (len = harr.length; i < len; i++)
 						harr[i] = harr[i + 1]
 					harr.length--
-					
-					count = 0
-					for (k in handlers)
-						count += handlers[k].length
-					
-					if (!count)
-						this.dispatchEvent = uselessDispatchEvent
 				}
 			}
 	},
 	
-	dispatchEvent: uselessDispatchEvent
+	dispatchEvent: function (e, data)
+	{
+		var handlers, harr, len
+		if ((handlers = this[handlersProp]))
+			if ((harr = handlers[e.type]))
+				if ((len = harr.length))
+					for (var i = 0; i < len; i++)
+						try { harr[i].call(this, e) }
+						catch (ex) { log(ex) }
+		
+		return !e.defaultPrevented
+	},
+	
+	dispatchEventData: function (type, data)
+	{
+		var e = doc.createEvent()
+		e.initEvent(type)
+		if (data !== undef)
+			e.data = data
+		
+		return this.dispatchEvent(e)
+	}
 }
 
 })();
