d008d33586fe06f0acf50cd8e4a1caa0ae4b5285
Peter Leonov <peter@kung-fu-tzu.ru>
2009-09-03 03:37:33 +0400

    easing is split out from animation

---
+++ js/0.3/modules/easing.js
@@ -0,0 +1,86 @@
+;(function () {
+
+var Me = Programica.Easing = function (begin, end, motion, type, callback)
+{
+	var me = this, frame = 0, total = duration * this.fps, last = total - 1,
+		delta = end - begin, step = delta / total
+	
+	this.tick = function ()
+	{
+		if (frame++ >= last)
+			me.stop()
+		callback(motion(frame, begin, delta, last))
+	}
+	
+}
+
+Me.prototype =
+{
+	start: function ()
+	{
+		if (!this.running)
+		{
+			this.running = true
+			Me.removeTimer(this.timer)
+			this.timer = Me.addTimer(this.tick) // step is already a prepared callback
+		}
+		return this
+	},
+	
+	stop: function ()
+	{
+		if (this.running)
+		{
+			this.running = false
+			Me.removeTimer(this.timer)
+		}
+		return this
+	},
+	
+	
+}
+
+
+// one global timer implementation
+
+Me.fps = 60
+var timers = Me.timers = {}
+Me.total = 0
+Me.id = 0
+
+Me.tick = function (d)
+{
+	var timer
+	for (var k in timers)
+		if (t = timers[k])
+			timer(d)
+}
+
+Me.addTimer = function (callback)
+{
+	// if was no timers
+	if (this.total++ <= 0)
+	{
+		var t = this
+		this.timer = setInterval(this.tick, 1000 / this.fps)
+	}
+	
+	this.timers[++this.id] = callback
+	return this.id
+}
+
+Me.removeTimer = function (id)
+{
+	if (this.timers[id])
+	{
+		delete this.timers[id]
+		
+		// if have deleted last timer
+		if (--this.total <= 0)
+			clearInterval(this.timer)
+	}
+}
+
+Me.types = <!--#include virtual="easing-types.js" -->
+
+})();
