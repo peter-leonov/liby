dd368626bcd2a992b30f636a6d5bb63fb95b32d6
Peter Leonov <peter@kung-fu-tzu.ru>
2009-09-03 05:12:28 +0400

    massive animation rewrite

--- js/0.3/modules/animation.js
+++ js/0.3/modules/animation.js
@@ -1,93 +1,85 @@
 ;(function () {
 
-var PA = Programica.Animation = function (prms)
+var Easing = Programica.Easing, Me = Programica.Animation = function (node, motion, duration, trans, unit)
 {
-	prms = prms || {}
-
-	// defaults
-	this.trans	= []
-	this.duration			= prms.duration || 1,
-	this.unit				= prms.unit != null ? prms.unit : PA.defaults.unit
-	this.motion				= prms.motion || PA.defaults.type
-	this.running			= false
-	this.complete			= false
-	this.node				= prms.node || false
-	this.animationTypes		= PA.Types
-	
-	this.onstart = this.oncomplete = this.onstep = function () {}
-	
-	switch (typeof this.motion)
+	this.node = node
+	switch (typeof motion)
 	{
 		case 'string':
-			this.motion = this.animationTypes[this.motion] || null
+			var name = motion
+			if (!(motion = Easing.motions[name]))
+				throw new Error('Unknown easing type name "' + name + '"')
 			break
 		case 'function':
 			break
 		default:
-			this.motion = null
+			throw new Error('Easing type must be a string or a function, got "' + typeof motion + '"')
+	}
+	this.motion = motion
+	this.duration = duration
+	this.trans = trans
+	this.unit = unit
+	this.running = false
+	this.complete = false
+	this.easings = []
+	
+	var me = this
+	function complete () { me.complete() }
+	for (var i = 0; i < trans.length; i++)
+	{
+		var tr = trans[i]
+		function tick (value)
+		{
+			me.onstep()
+			Me.setStyleProperty(node, tr.property, value, unit)
+		}
+		this.easings[i] = new Easing(tr.begin, tr.end, duration, motion, tick, complete)
 	}
 	
-	if (prms.trans)
-		for (var i = 0; i < prms.trans.length; i++)
-			if (prms.trans[i])
-				this.trans.push(prms.trans[i])
 }
+
+Me.defaults = {unit: 'px', motion: 'linearTween', duration: 1}
 
 Element.prototype.animate = function (motion, props, duration, unit)
 {
+	var defaults = Me.defaults
+	if (!motion)
+		motion = defaults.motion
+	if (!duration)
+		duration = defaults.duration
+	if (!unit)
+		unit = defaults.unit
+	
 	var trans = []
-	for (var i in props)
+	for (var k in props)
 	{
-		var pi = props[i]
-		if (pi.length == 2)
-			trans.push({property: i, begin: pi[0], end: pi[1]})
+		var prop = props[k]
+		if (prop.length == 2)
+			trans.push({property: k, begin: prop[0], end: prop[1]})
 		else
-			trans.push({property: i, begin: null, end: pi[0] || pi})
+			trans.push({property: k, begin: Me.getStyleProperty(this, k), end: prop[0] || prop})
 	}
-	return new PA({node: this, motion: motion, duration: duration, trans: trans, unit: unit}).start()
+	return new Me(this, motion, duration, trans, unit).start()
 }
 
 
-PA.fps = 60
-PA.defaults = {unit: 'px', type: 'linearTween'}
-
-PA.prototype =
+Me.prototype =
 {
+	onstart: function () {},
+	oncomplete: function () {},
+	onstep: function () {},
 	start: function ()
 	{
 		// if animation is already started
 		if (this.running)
 			return this
 		
-		// only one animation per node for now
-		if (this.node.animation)
-			this.node.animation.stop()
-		this.node.animation = this
-		
 		this.running = true
 		this.complete = false
 		
-		var t = this
-		if (this.motion == this.animationTypes.directJump)
-		{
-			this.frame = this.totalFrames - 1
-			setTimeout(function () { t.renderForceLast(); t.stop(); t.complete = true; t.oncomplete() }, 0)
-		}
-		else
-		{
-			this.frame = 0
-			this.totalFrames = this.duration * PA.fps
-			
-			for (var i = 0; i < this.trans.length; i++)
-			{
-				var tr = this.trans[i]
-				if (tr.begin == null)
-					tr.begin = this.getStyleProperty(tr.property)
-				tr.step = ( tr.end - tr.begin ) / this.totalFrames
-			}
-			
-			this.timer = PA.addTimer( function () { t.step() } )
-		}
+		var easings = this.easings
+		for (var i = 0; i < easings.length; i++)
+			easings[i].start()
 		
 		this.onstart()
 		return this
@@ -97,135 +89,68 @@ PA.prototype =
 	{
 		if (this.running)
 		{
-			this.node.animation = null
 			this.running = false
-			PA.removeTimer(this.timer)
+			
+			var easings = this.easings
+			for (var i = 0; i < easings.length; i++)
+				easings[i].stop()
 		}
 		
 		return this
-	},
-	
-	step: function ()
-	{
-		this.render()
-		this.onstep()
-		
-		if (this.frame >= this.totalFrames - 1)
-		{
-			this.stop()
-			this.complete = true
-			this.oncomplete()
-		}
-		
-		this.frame++
-	},
-	
-	renderForceLast: function ()
-	{
-		for (var i = 0; i < this.trans.length; i++)
-		{
-			var t = this.trans[i]
-			this.setStyleProperty(t.property, t.end)
-		}
-	},
-	
-	render: function ()
-	{
-		for (var i = 0; i < this.trans.length; i++)
-		{
-			var t = this.trans[i]
-			if ( t.property != null && t.begin != null && t.end != null  )
-				this.setStyleProperty(t.property, this.motion(this.frame + 1, t.begin, t.end - t.begin, this.totalFrames))
-			else
-				throw new Error("Corupted transformation: " + t)
-		}
-	},
+	}
+}
 
-	getStyleProperty: function (p)
+Me.getStyleProperty = function (node, p)
+{
+	if (p == "top" && !node.style[p])
+		return node.offsetTop
+	
+	if (p == "left" && !node.style[p])
+		return node.offsetLeft
+	
+	
+	if (p == "opacity" && isNaN(parseFloat(node.style[p])))
+		return 1
+	
+	
+	if (/scroll/.test(p))
+		return node[p]
+	
+	return parseFloat(node.style[p]) || 0
+},
+
+Me.setStyleProperty = function (node, p, value, unit)
+{
+	try
 	{
-		if (p == "top" && !this.node.style[p])
-			return this.node.offsetTop
+		if (/color/.test(p))
+			return node.style[p] = 'rgb(' + parseInt(value) + ',' + parseInt(value) + ',' + parseInt(value) + ')'
 		
-		if (p == "left" && !this.node.style[p])
-			return this.node.offsetLeft
-		
-		
-		if (p == "opacity" && isNaN(parseFloat(this.node.style[p])))
-			return 1
+		// for SVG elements
+		if (p == 'r')
+			return node.r.baseVal.value = value
 		
 		
 		if (/scroll/.test(p))
-			return this.node[p]
+			return node[p] = Math.round(value)
 		
-		return parseFloat(this.node.style[p]) || 0
-	},
-	
-	setStyleProperty: function (p, value)
+		if (p == 'opacity')
+			return node.style[p] = value
+		
+		if ((p == 'width' || p == 'height') && value < 0)
+			value = 0
+		
+		if (unit == 'em')
+			return node.style[p] = Math.round(value * 100) / 100 + unit
+		
+		return node.style[p] = Math.round(value) + unit
+	}
+	catch (ex)
 	{
-		try
-		{
-			if (/color/.test(p))
-				return this.node.style[p] = 'rgb(' + parseInt(value) + ',' + parseInt(value) + ',' + parseInt(value) + ')'
-			
-			// for SVG elements
-			if (p == 'r')
-				return this.node.r.baseVal.value = value
-			
-			
-			if (/scroll/.test(p))
-				return this.node[p] = Math.round(value)
-			
-			if (p == "opacity")
-				return this.node.style[p] = value
-			
-			if ((p == 'width' || p == 'height') && value < 0)
-				value = 0
-			
-			if (this.unit == 'em')
-				return this.node.style[p] = Math.round(value * 100) / 100 + this.unit
-			
-			return this.node.style[p] = Math.round(value) + this.unit
-		}
-		catch (ex)
-		{
-			log(ex + p + value)
-			return value
-		}
+		log(ex + p + value)
+		return value
 	}
 }
 
-PA.addTimer = function (func)
-{
-	if (!this.timer)
-	{
-		var t = this
-		this.timer = setInterval(function (d) { t.time(d) }, 1000 / this.fps)
-	}
-	
-	return this.timers.push(func)
-}
-
-PA.removeTimer = function (num)
-{
-	var ts = this.timers
-	ts[num-1] = null
-	
-	while (ts[ts.length-1] === null)
-		ts.length--
-	
-	if (!ts.length)
-		clearInterval(this.timer), this.timer = null
-}
-
-PA.timers = []
-
-PA.time = function (d)
-{
-	for (var i = 0; i < this.timers.length; i++)
-		this.timers[i] && this.timers[i](d)
-}
-
-
-<!--#include virtual="animation-types.js" -->
 
 })();
