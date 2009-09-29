3e12f467ce39e82b2a35fa60c9983159d8924f95
Peter Leonov <peter@kung-fu-tzu.ru>
2009-09-29 12:29:55 +0400

    not-a-string-passed workaround for FF 3.5 RegExp#test() fix

--- js/0.3/programica/fixes/gecko.js
+++ js/0.3/programica/fixes/gecko.js
@@ -12,5 +12,5 @@ else
 if (!/привет/i.test("Привет"))
 {
 	RegExp.prototype.__pmc_test = RegExp.prototype.test
-	RegExp.prototype.test = function (str) { return this.__pmc_test(this.ignoreCase ? str.toLowerCase() : str) }
+	RegExp.prototype.test = function (str) { return this.__pmc_test(this.ignoreCase ? String(str).toLowerCase() : str) }
 }
