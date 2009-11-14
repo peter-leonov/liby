6f6bdc3ed39e627eabcef77c7bac2867d1113f88
Peter Leonov <peter@kung-fu-tzu.ru>
2009-11-14 16:58:32 +0300

    mess around

--- js/0.1/Programica.lite.js
+++
@@ -1,4 +0,0 @@
-<!--#include file="Programica/Init.js" -->
-<!--#include file="Programica/Fixes.js" -->
-<!--#include file="Programica/DOM.js" -->
-<!--#include file="Programica/Animation.js" -->
--- js/0.2/Programica/fixes.js
+++
@@ -1,9 +0,0 @@
-<!--#if expr="$HTTP_USER_AGENT = /Gecko\//" --><!--#include virtual="fixes/Gecko.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /Opera\//" --><!--#include virtual="fixes/Presto.js" --><!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /WebKit\//" --><!--#include virtual="fixes/WebKit.js" --><!--#endif -->
-
-<!--#if expr="$HTTP_USER_AGENT = /MSIE [67]/" -->
-<!--#include virtual="fixes/Trident.XHR.js" -->
-<!--#include virtual="fixes/Trident.js" -->
-<!--#endif -->
-<!--#if expr="$HTTP_USER_AGENT = /MSIE 8/" --><!--#include virtual="fixes/Trident8.js" --><!--#endif -->
