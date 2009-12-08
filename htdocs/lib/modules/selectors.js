8e7c8ebbd1bd16edb2a12262cb509a828be9fd8c
Peter Leonov <peter@kung-fu-tzu.ru>
2009-12-09 00:10:49 +0300

    laconic gebcn

--- htdocs/lib/modules/selectors.js
+++ htdocs/lib/modules/selectors.js
@@ -6,36 +6,18 @@ window.$$ = Sizzle
 
 if (!document.getElementsByClassName)
 {
-	// from prototype 1.5.1.1
 	Element.prototype.getElementsByClassName = document.getElementsByClassName = function (className, tagName)
 	{
-		// geting elems with native function
-		var children = this.getElementsByTagName(tagName || '*')
-		// predeclaring vars
-		var elements = [], child, l = 0
-		// precompile regexp
-		var rex = new RegExp("(?:\\s+|^)" + className + "(?:\\s+|$)")
-		// length is constant, so caching its value
-		var len = children.length
+		var children = this.getElementsByTagName(tagName || '*'),
+			rex = new RegExp("(?:\\s+|^)" + className + "(?:\\s+|$)")
 		
-		// memory for array of nodes will be allocated only once
-		elements.length = len
-		for (var i = 0; i < len; i++)
+		var res = []
+		for (var l = 0, i = 0, il = children.length; i < il; i++)
 		{
-			// even caching the reference for children[i] gives us some nanoseconds ;)
-			child = children[i]
-			// just rely on RegExp engine
-			if (rex.test(child.className))
-			{
-				// simple assignment
-				elements[l] = child
-				// simple increment
-				l++
-			}
+			if (rex.test(children[i].className))
+				res[l++] = children[i]
 		}
-		// truncating garbage length
-		elements.length = l
 		
-		return elements
+		return res
 	}
 }
