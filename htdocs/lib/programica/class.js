046e67e32a57a000e5a5889d2e7b9036c5f34967
Peter Leonov <peter@kung-fu-tzu.ru>
2009-12-09 16:56:30 +0300

    simplified module logic + a few tests

--- htdocs/lib/programica/class.js
+++ htdocs/lib/programica/class.js
@@ -23,29 +23,33 @@ Me.Object.prototype = {}
 
 
 
-// Module
 ;(function(){
 
-var myName = 'Module'
-var Me = self[myName] = function (name, proto)
+var myName = 'Module', count = 0,
+	Me = self[myName] = function ()
+	{
+		this.links = []
+	}
+
+Me.prototype =
 {
-	var module = function () { throw new Error(myName + ': can`t create direct instances of myself') }
-	module.className = name || '[anonimous ' + myName + ']'
-	module.constructor = Me
-	module.mix = function (cls) { Object.extend(cls.prototype, this.prototype); return this }
-	
-	if (proto)
-		module.prototype = proto
+	mix: function (cls)
+	{
+		Object.add(cls.prototype, this.prototype)
+		return this
+	}
+}
+
+Me.create = function (name, proto)
+{
+	var module = new Me()
+	module.className = name || '[anonymous ' + myName + ' ' + ++count + ']',
+	module.prototype = proto || {}
 	return module
 }
 
 Me.className = myName
 
-Function.prototype.mixIn = function (module)
-{
-	if (module.constructor != Me)
-		throw new Error('Function: can only mixIn modules')
-	return module.mix(this)
-}
+Function.prototype.mixIn = function (module) { return module.mix(this) }
 
 })();
