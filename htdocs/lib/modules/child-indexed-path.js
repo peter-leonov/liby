9c803b327129e1ca491740b34c0341bf35dd00bf
Peter Leonov <gojpeg@gmail.com>
2009-12-19 03:36:02 +0300

    Element#childIndexedPath() added (extracted from event recorder)

---
+++ htdocs/lib/modules/child-indexed-path.js
@@ -0,0 +1,40 @@
+;(function(){
+
+var myProto =
+{
+	childIndexedPath: function (node)
+	{
+		var path = []
+		for (;;)
+		{
+			if (node == this)
+				break
+			
+			var parent = node.parentNode
+			if (!parent)
+				return null
+			
+			var childs = parent.childNodes
+			for (var i = 0, num = 0, il = childs.length; i < il; i++)
+			{
+				var child = childs[i]
+				if (child == node)
+					break
+				// lets count only the elements
+				else if (child.nodeType == 1)
+					num++
+			}
+			
+			path.push(num)
+			node = parent
+		}
+		
+		return path.reverse()
+	}
+}
+
+Object.add(Element.prototype, myProto)
+Object.add(document, myProto)
+
+
+})();
