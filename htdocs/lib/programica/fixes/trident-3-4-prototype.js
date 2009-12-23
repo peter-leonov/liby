70f64d2774d70ee52c7999385d2d5134111a0fd5
Peter Leonov <gojpeg@gmail.com>
2009-12-23 18:20:20 +0300

    fixed typo in IE prototype fixes

--- htdocs/lib/programica/fixes/trident-3-4-prototype.js
+++ htdocs/lib/programica/fixes/trident-3-4-prototype.js
@@ -1,5 +1,5 @@
 (function(){
 
-Array.copy = function (s) { var d = []; if (s !== undef) for (var i = 0, len = s.length; i < len; i++) d[i] = s[i]; return d }
+Array.copy = function (s) { var d = []; if (s !== undefined) for (var i = 0, len = s.length; i < len; i++) d[i] = s[i]; return d }
 
 })();
