c0186644c8c05da979d1d2b32414d399f41487ee
Peter Leonov <gojpeg@gmail.com>
2010-01-14 14:21:06 +0300

    no more buggy base href

--- htdocs/lib/tests/tests.common.js
+++ htdocs/lib/tests/tests.common.js
@@ -3,10 +3,10 @@
 function script (src) { document.write('<script src="' + src + '" type="text/javascript"></script>') }
 function style (src) { document.write('<link rel="stylesheet" href="' + src + '" type="text/css"/>') }
 
-script('../modules/cascade.js')
-script('../modules/test.js')
-script('../widgets/tests.js')
+script('/lib/modules/cascade.js')
+script('/lib/modules/test.js')
+script('/lib/widgets/tests.js')
 
-style('tests.css')
+style('/lib/tests/tests.css')
 
 })();
