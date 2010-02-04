7770f58b3f39850f0c9f04ef810c27d1cfa924cb
Peter Leonov <gojpeg@gmail.com>
2010-02-04 19:54:18 +0300

    less annoying ga.js including

--- htdocs/lib/tests/tests.common.js
+++ htdocs/lib/tests/tests.common.js
@@ -11,21 +11,5 @@ style('/lib/tests/tests.css')
 
 })();
 
-;(function(){
-
-function loadTracker (e)
-{
-	var _gaq = window._gaq = [];
-	_gaq.push(['_setAccount', 'UA-XXXXX-X']);
-	_gaq.push(['_trackPageview']);
-	
-	var ga = document.createElement('script')
-	ga.type = 'text/javascript'
-	ga.async = true
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga)
-}
-
-setTimeout(loadTracker, 2000)
-
-})();
+<!--# block name="ga" -->/* no ga.js was found */<!--# endblock -->
+<!--# include virtual="/js/ga.js" stub="ga" -->
