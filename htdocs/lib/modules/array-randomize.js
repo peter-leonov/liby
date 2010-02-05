fc6a4e572130dbf54f314e0681594bbfcabdced0
Peter Leonov <gojpeg@gmail.com>
2010-02-05 16:03:05 +0300

    Array#random(n)

--- htdocs/lib/modules/array-randomize.js
+++ htdocs/lib/modules/array-randomize.js
@@ -4,16 +4,36 @@ var random = Math.random
 
 Array.prototype.randomize = function ()
 {
-	for (var v, i = 0, il = this.length; i < il; i++)
+	for (var i = 0, il = this.length; i < il; i++)
 	{
 		var j = (random() * il) >> 0
-		v = this[j]
+		var v = this[j]
 		this[j] = this[i]
 		this[i] = v
-		// t.log(j)
 	}
 	
 	return this
+}
+
+Array.prototype.random = function (n)
+{
+	var len = this.length
+	
+	if (n > len)
+		n = len
+	
+	if (n > 1)
+	{
+		var copy = this.slice()
+		copy.randomize()
+		return n === len ? copy : copy.slice(0, n)
+	}
+	
+	if (n === 1)
+		return [this[(random() * len) >> 0]]
+	
+	// n <= 0
+	return []
 }
 
 })();
