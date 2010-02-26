fdf6d43e72037bb98511a4fc01151144f2cb87db
Peter Leonov <gojpeg@gmail.com>
2010-02-26 15:49:00 +0300

    Array#indexOf() and conditional extending

--- htdocs/lib/core/fixes/trident-3-4-prototype.js
+++ htdocs/lib/core/fixes/trident-3-4-prototype.js
@@ -1,5 +1,29 @@
 (function(){
 
+if (!Array.copy)
 Array.copy = function (s) { var d = []; if (s !== undefined) for (var i = 0, len = s.length; i < len; i++) d[i] = s[i]; return d }
+
+if (!Array.prototype.indexOf)
+Array.prototype.indexOf = function (v, i)
+{
+	var len = this.length
+	
+	if ((i = +i))
+	{
+		if (i < 0)
+			i = ceil(i) + len
+		else
+			i = floor(i)
+	}
+	else
+		i = 0
+	
+	for (; i < len; i++)
+		if (i in this && this[i] === v)
+			return i
+	
+	return -1
+}
+
 
 })();
