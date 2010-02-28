2c42eef8937f2f0a6d17c8246b95abbc52fdc56c
Peter Leonov <gojpeg@gmail.com>
2010-02-28 20:03:14 +0300

    new Oops module with dependance on Tracker (may be one provided by GA)

---
+++ htdocs/lib/modules/oops.js
@@ -0,0 +1,62 @@
+// at this stage no fixes or wrappers are loaded from any lib
+// except for Tracker object that hmm… tracks :)
+;(function(){
+
+var myName = 'Oops'
+
+var Me =
+{
+	enabled: false,
+	
+	onerror: function (message, url, line)
+	{
+		this.report('error', url + ':' + line + ': ' + message)
+	},
+	
+	report: function (type, message)
+	{
+		try // to fully describe an error
+		{
+			Tracker.report('Oops', type, message)
+		}
+		catch (ex)
+		{
+			this.log('could not report')
+		}
+		
+		return true
+	},
+	
+	maybeEnable: function ()
+	{
+		if (!/Oops=disabled/.test(document.cookie))
+			this.enable()
+	},
+	
+	enable: function ()
+	{
+		if (this.enabled)
+			return
+		this.enabled = true
+		
+		window.onerror = Me.onerror
+		this.log('error catching enabled')
+	},
+	
+	disable: function ()
+	{
+		if (!this.enabled)
+			return
+		this.enabled = false
+		
+		window.onerror = null
+		this.log('error catching disabled')
+	},
+	
+	log: function (str) { try { console.log('Oops: ' + str) } catch (ex) {} }
+}
+
+self.className = Me
+self[myName] = Me
+
+})();
