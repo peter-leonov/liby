86fca8063d85f8ccde0d3f171e629c46dacf58d8
Peter Leonov <gojpeg@gmail.com>
2010-03-17 11:30:52 +0300

    more reliable fix (no race condition on page load/ready)

--- htdocs/lib/core/fixes/body-offset-parent.js
+++ htdocs/lib/core/fixes/body-offset-parent.js
@@ -1,2 +1,2 @@
 if (/Opera\/(9\.[5-9]|10\.)/.test(navigator.userAgent))
-	window.addEventListener('load', function (e) { document.body.offsetParent = document.documentElement }, false)
+	document.body.offsetParent = document.documentElement
