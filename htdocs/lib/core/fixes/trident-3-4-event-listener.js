d3dcb2e07bbea14c405935576cd56ac1e25547fd
Peter Leonov <gojpeg@gmail.com>
2010-04-01 15:02:49 +0400

    now we can rely on automatic GC

--- htdocs/lib/core/fixes/trident-3-4-event-listener.js
+++ htdocs/lib/core/fixes/trident-3-4-event-listener.js
@@ -5,19 +5,6 @@ var doc = document, docelem = doc.documentElement, win = window, undef
 if (win.addEventListener || !win.attachEvent)
 	return
 
-function onBeforeUnload ()
-{
-	var stack = onBeforeUnload.stack
-	for (var i = 0, il = stack.length; i < il; i++)
-	{
-		var el = stack[i]
-		el[0].detachEvent(el[1], el[2])
-	}
-	onBeforeUnload.stack = []
-}
-onBeforeUnload.stack = []
-window.attachEvent('onbeforeunload', onBeforeUnload)
-
 var eventTransport = doc.__pmc__eventTransport = 'beforeeditfocus',
 	supportedEvents = {abort:1, activate:1, afterprint:1, afterupdate:1, beforeactivate:1, beforecopy:1, beforecut:1, beforedeactivate:1, beforeeditfocus:1, beforepaste:1, beforeprint:1, beforeunload:1, beforeupdate:1, blur:1, bounce:1, cellchange:1, change:1, click:1, contextmenu:1, controlselect:1, copy:1, cut:1, dataavailable:1, datasetchanged:1, datasetcomplete:1, dblclick:1, deactivate:1, drag:1, dragend:1, dragenter:1, dragleave:1, dragover:1, dragstart:1, drop:1, error:1, errorupdate:1, filterchange:1, finish:1, focus:1, focusin:1, focusout:1, hashchange:1, help:1, keydown:1, keypress:1, keyup:1, layoutcomplete:1, load:1, losecapture:1, message:1, mousedown:1, mouseenter:1, mouseleave:1, mousemove:1, mouseout:1, mouseover:1, mouseup:1, mousewheel:1, move:1, moveend:1, movestart:1, offline:1, online:1, page:1, paste:1, progress:1, propertychange:1, readystatechange:1, reset:1, resize:1, resizeend:1, resizestart:1, rowenter:1, rowexit:1, rowsdelete:1, rowsinserted:1, scroll:1, select:1, selectionchange:1, selectstart:1, start:1, stop:1, storage:1, storagecommit:1, submit:1, timeout:1, unload:1}
 
