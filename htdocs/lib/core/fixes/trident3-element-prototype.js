2738e174418d4b984923e5ea9de7e015c6651c57
Peter Leonov <gojpeg@gmail.com>
2010-04-02 21:09:22 +0400

    copy-paste is no good (even has done in the name of performance)

--- htdocs/lib/core/fixes/trident3-element-prototype.js
+++ htdocs/lib/core/fixes/trident3-element-prototype.js
@@ -4,14 +4,8 @@ var doc = document, win = window, Ep = win.Element.prototype
 
 function fixNodes (nodes)
 {
-	var proto = Ep
 	for (var i = 0, il = nodes.length; i < il; i++)
-	{
-		var node = nodes[i]
-		
-		for (var p in proto)
-			node[p] = proto[p]
-	}
+		fixNode(nodes[i])
 }
 
 function fixNode (node)
@@ -19,13 +13,16 @@ function fixNode (node)
 	var proto = Ep
 	for (var p in proto)
 		node[p] = proto[p]
-	
-	return node
 }
 
 
 doc.__pmc__createElement = doc.createElement
-doc.createElement = function (type) { return fixNode(doc.__pmc__createElement(type)) }
+doc.createElement = function (type)
+{
+	var node = doc.__pmc__createElement(type)
+	fixNode(node)
+	return node
+}
 
 // events must be fixed at this point to preserve handlers call order
 win.addEventListener('load', function () { fixNodes(doc.all) })
