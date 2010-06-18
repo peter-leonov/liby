1962e8ec4b187302ce4bbef1dfa1ea47dfb531ad
Peter Leonov <gojpeg@gmail.com>
2010-06-18 23:42:03 +0400

    start passes all tests

--- htdocs/lib/core/fixes/trident3-input-selection.js
+++ htdocs/lib/core/fixes/trident3-input-selection.js
@@ -3,35 +3,75 @@
 // selection logic from:
 // http://www.webdeveloper.com/forum/showthread.php?t=74982
 
-var selection = document.selection
+var selection = document.selection,
+	Me = HTMLInputElement
+
+function setStart (node, pos)
+{
+	// jump to the end
+	node.focus()
+	
+	var sel = selection.createRange()
+	
+	// jump to the begin
+	sel.moveStart('character', -node.value.length)
+	
+	// jump to set postion
+	sel.moveStart('character', pos)
+	sel.moveEnd('character', 0)
+	sel.select()
+}
+
+var internal = false
 
 function setter ()
 {
+	if (internal)
+		return
+	internal = true
+	
 	var e = event,
 		node = e.srcElement,
 		name = e.propertyName,
-		value = node[e.propertyName]
+		v = node[e.propertyName]
 	
-	if (name == 'selectionStart')
+	if (name == 'value')
 	{
-		// jump to the end
-		node.focus()
-		
-		var sel = selection.createRange()
-		
-		// jump to the begin
-		sel.moveStart('character', -node.value.length)
-		
-		// jump to set postion
-		sel.moveStart('character', value)
-		sel.moveEnd('character', 0)
-		sel.select()
+		var len = v.length
+		node.selectionStart = len
+		node.selectionEnd = len
 	}
+	else if (name == 'selectionStart')
+	{
+		v >>= 0
+		if (v < 0)
+			v = 0
+		else
+		{
+			var len = node.value.length
+			if (v > len)
+				v = len
+		}
+		
+		node.selectionStart = v
+		
+		try
+		{
+			setStart(node, v)
+		}
+		catch (ex) { setTimeout(function () { throw ex }, 10) }
+		
+	}
+	
+	internal = false
 }
 
-HTMLInputElement.__pmc_fixHook = function (node)
+Me.__pmc_fixHook = function (node)
 {
 	node.attachEvent('onpropertychange', setter)
 }
+
+Me.prototype.selectionStart = 0
+Me.prototype.selectionEnd = 0
 
 })();
