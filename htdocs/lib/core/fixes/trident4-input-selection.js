122f7f85a54daf8ac86dfb67db4679bd0d8b30e3
Peter Leonov <gojpeg@gmail.com>
2010-06-21 13:44:11 +0400

    much simpler fix for IE8

---
+++ htdocs/lib/core/fixes/trident4-input-selection.js
@@ -0,0 +1,65 @@
+;(function(){
+
+var selection = document.selection,
+	Me = HTMLInputElement
+
+// var internal = false
+
+var selectionStart =
+{
+	get: function ()
+	{
+		// create a range copy
+		var r = selection.createRange().duplicate()
+		
+		// move end to the end of the input
+		r.moveEnd('character', 100000)
+		
+		return this.value.length - r.text.length
+	},
+	
+	set: function (v)
+	{
+		// jump to the input if not already in it
+		this.focus()
+		
+		// create a range within the input
+		var r = selection.createRange()
+		
+		// move start and end to the begin of the input
+		r.moveStart('character', v - this.selectionStart)
+		r.select()
+	}
+}
+
+var selectionEnd =
+{
+	get: function ()
+	{
+		// create a range copy
+		var r = selection.createRange().duplicate()
+		
+		// move start to the begin of the input
+		r.moveStart('character', -100000)
+		
+		return r.text.length
+	},
+	
+	set: function (v)
+	{
+		// jump to the input if not already in it
+		this.focus()
+		
+		// create a range within the input
+		var r = selection.createRange()
+		
+		// move start and end to the begin of the input
+		r.moveEnd('character', v - this.selectionEnd)
+		r.select()
+	}
+}
+
+Object.defineProperty(Me.prototype, "selectionStart", selectionStart)
+Object.defineProperty(Me.prototype, "selectionEnd", selectionEnd)
+
+})();
