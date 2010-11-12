f99768610ad341f99dd545e2d7919b128cee2e70
Peter Leonov <gojpeg@gmail.com>
2010-11-13 00:54:45 +0300

    use ../src/ prefix

--- tests/tests.common.js
+++ tests/tests.common.js
@@ -3,12 +3,11 @@
 function script (src) { document.write('<script src="' + src + '" type="text/javascript"></script>') }
 function style (src) { document.write('<link rel="stylesheet" href="' + src + '" type="text/css"/>') }
 
-script('/lib/modules/cascade.js')
-script('/lib/modules/test.js')
-script('/lib/modules/test-tool.js')
-script('/lib/widgets/tests.js')
-script('/js/ga.js')
+script('../src/modules/cascade.js')
+script('../src/modules/test.js')
+script('../src/modules/test-tool.js')
+script('../src/widgets/tests.js')
 
-style('/lib/tests/tests.css')
+style('tests.css')
 
 })();
