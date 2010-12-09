41049e655f70c9ce18b3e2eb739b5564bfa95310
Peter Leonov <gojpeg@gmail.com>
2010-12-09 16:51:56 +0300

    comment the every step

--- src/modules/tracker-ga.js
+++ src/modules/tracker-ga.js
@@ -3,24 +3,28 @@
 if (!window._gaq)
 	window._gaq = []
 
+// As far as Tracker could be used as error reporter
+// wrap everything in try/catch blocks to avoid
+// infinite reporting on error in Tracker itself.
+
 var Me =
 {
 	track: function (category, action, label, value)
 	{
-		try
+		try // to track an event
 		{
 			window._gaq.push(['_trackEvent', category, action, label, value])
 			return true
 		}
 		catch (ex)
 		{
-			try
+			try // to warn the developer
 			{
 				console.log(this.className + ': could not report a track')
 			}
 			catch (ex)
 			{
-				
+				// nothing to do
 			}
 		}
 	}
