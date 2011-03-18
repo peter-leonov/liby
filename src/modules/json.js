d2c14b1c33ecf2466b874b2dfc16a92ca3ebe07d
Peter Leonov <gojpeg@gmail.com>
2011-03-18 19:36:41 +0300

    don't touch the Unicode

--- src/modules/json.js
+++ src/modules/json.js
@@ -6,7 +6,6 @@ if (!self.JSON)
 function f (n) { return n < 10 ? '0' + n : n }
 
 var myName = 'JSON', Me = self[myName] = {},
-cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
 escapeable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
 Object_hasOwnProperty = Object.hasOwnProperty,
 Array_constructor = Array,
@@ -90,11 +89,6 @@ function str (value)
 
 function parse (text)
 {
-	cx.lastIndex = 0;
-	if (cx.test(text))
-		text = text.replace(cx, function (a) { return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4) })
-	
-	
 	parse.lastError = null
 	if (/^[\],:{}\s]*$/.
 		test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@').
