40348e6134d32142a07b0ac613e621828b2d2ba1
Peter Leonov <gojpeg@gmail.com>
2011-03-19 00:39:27 +0300

    port window.name simple storage

---
+++ src/modules/window-name.js
@@ -0,0 +1,25 @@
+;(function(){
+
+var Me =
+{
+	get: function (key)
+	{
+		var hash = UrlEncode.parse(decodeURIComponent(window.name))
+		return hash ? hash[key] : undefined
+	},
+	
+	set: function (key, val)
+	{
+		var hash = UrlEncode.parse(decodeURIComponent(window.name)) || {}
+		hash[key] = val
+		window.name = encodeURIComponent(UrlEncode.stringify(hash))
+		return val
+	},
+	
+	clear: function () { window.name = "" }
+}
+
+Me.className = 'WindowName'
+self[Me.className] = Me
+
+})();
