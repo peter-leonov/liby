347360c6506c78d6f6804755add5c5c00ba74137
Peter Leonov <gojpeg@gmail.com>
2012-03-26 19:57:53 +0400

    fix log() in MSIE 8

--- src/modules/log.js
+++ src/modules/log.js
@@ -5,9 +5,13 @@ var s = self
 if (!s.log)
 {
 	// console object present
-	if (s.console)
-		s.log = function () { s.console.log.apply(s.console, arguments) }
-	
+	if (s.console && s.console.log)
+	{
+		if (s.console.log.apply)
+			s.log = function () { s.console.log.apply(s.console, arguments) }
+		else
+			s.log = s.console.log
+	}
 	// Opera
 	else if (s.opera && s.opera.postError)
 		s.log = function () { return s.opera.postError(arguments) }
