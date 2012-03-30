57de730636af003c0945bff326982161781c6f54
Peter Leonov <gojpeg@gmail.com>
2012-03-30 13:53:09 +0400

    Date.parseDiff()

--- src/modules/date.js
+++ src/modules/date.js
@@ -11,4 +11,21 @@ D.year = 365 * 864e5
 if (!D.now)
 	D.now = function () { return +new D() }
 
+var map =
+{
+	s: 'second',
+	m: 'minute',
+	h: 'hour',
+	d: 'day'
+}
+
+D.parseDiff = function (str)
+{
+	var m = /^([+-])?(\d+(?:\.\d+)?)([smhdy])$/.exec(str)
+	if (!m)
+		return null
+	
+	return [m[1] || '+', +m[2], map[m[3]]]
+}
+
 })();
