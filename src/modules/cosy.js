265de07bb967fbec02ed510e4d07c17eedece82d
Peter Leonov <gojpeg@gmail.com>
2012-05-05 19:00:48 +0400

    move $() and $$() from selectors to cosy.js

--- src/modules/cosy.js
+++ src/modules/cosy.js
@@ -1,3 +1,22 @@
+;(function(){
+
+function list2array (list)
+{
+	var arr = []
+	for (var i = 0, il = list.length; i < il; i++)
+		arr[i] = list[i]
+	return arr
+}
+
+window.$$ = function (query, root)
+{
+	var list = (root || document).querySelectorAll(query)
+	// if (!list || list.length == 0)
+	// 	alert('empty $$("' + query + '")')
+	return list2array(list)
+}
+window.$ = function (id) { return document.getElementById(id) }
+
 $.onload = function (f) { return window.addEventListener('load', f, false) }
 $.onready = function (f) { document.addEventListener('DOMContentLoaded', f, false) }
 $.load = function (src)
@@ -13,3 +32,5 @@ $.load = function (src)
 	cache[src] = node
 	return node
 }
+
+})();
