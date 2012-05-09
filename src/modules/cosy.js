576e1400ee236ff227a1733224847a1dc02f094a
Peter Leonov <gojpeg@gmail.com>
2012-05-09 13:04:39 +0400

    use Array.copy()

--- src/modules/cosy.js
+++ src/modules/cosy.js
@@ -1,19 +1,12 @@
 ;(function(){
 
-function list2array (list)
-{
-	var arr = []
-	for (var i = 0, il = list.length; i < il; i++)
-		arr[i] = list[i]
-	return arr
-}
-
 window.$$ = function (query, root)
 {
 	var list = (root || document).querySelectorAll(query)
 	// if (!list || list.length == 0)
 	// 	alert('empty $$("' + query + '")')
-	return list2array(list)
+	
+	return Array.copy(list)
 }
 window.$ = function (id) { return document.getElementById(id) }
 
