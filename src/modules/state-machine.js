349b01d8c1e5d8515d0f6dba595e4a49266e04e3
Peter Leonov <gojpeg@gmail.com>
2012-05-15 22:12:53 +0400

    state machine prototype :)

--- src/modules/state-machine.js
+++ src/modules/state-machine.js
@@ -7,7 +7,37 @@ function Me (papa)
 
 Me.prototype =
 {
+	setStates: function (states)
+	{
+		this.states = states
+		
+		for (var k in states)
+			states[k].stateName = k
+		
+		this.state = states.initial
+	},
 	
+	onswitch: function (from, to) {},
+	
+	switchState: function (name)
+	{
+		var transition = this.states[this.state.stateName + '_to_' + name]
+		if (transition)
+			transition.call(this.papa, this)
+		
+		var from = this.state.stateName
+		this.state = this.states[name]
+		var to = this.state.stateName
+		// log(from + ' -> ' + to)
+		this.onswitch.call(this.papa, from, to)
+		
+		this.exec()
+	},
+	
+	exec: function ()
+	{
+		this.state.call(this.papa, this)
+	}
 }
 
 Me.className = 'StateMachine'
