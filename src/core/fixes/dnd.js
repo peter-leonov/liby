2473b6e15fc9d293ac469af4c1475281398909cf
Peter Leonov <gojpeg@gmail.com>
2012-05-16 11:04:42 +0400

    first steps

--- src/core/fixes/dnd.js
+++ src/core/fixes/dnd.js
@@ -1,5 +1,51 @@
 (function(){
 
+var Me =
+{
+	states:
+	{
+		initial: function () {},
+		
+		waitForMouseDown: function (sm)
+		{
+			function listener (e)
+			{
+				this.startX = e.pageX
+				this.startY = e.pageY
+				
+				document.removeEventListener('mousedown', listener, false)
+				sm.switchState('waitForMoveFarEnough')
+			}
+			document.addEventListener('mousedown', listener, false)
+		},
+		
+		waitForMoveFarEnough: function (sm)
+		{
+			function listener (e)
+			{
+				if (Math.abs(this.startX - e.pageX) < 5 || Math.abs(this.startY - e.pageY) < 5)
+					return
+				
+				document.removeEventListener('mousemove', listener, false)
+				sm.switchState('startDrag')
+			}
+			document.addEventListener('mousemove', listener, false)
+		},
+		
+		startDrag: function ()
+		{
+			
+		}
+	},
+	
+	bind: function ()
+	{
+		var sm = this.sm = new StateMachine(this)
+		sm.setStates(this.states)
+		sm.switchState('waitForMouseDown')
+	}
+}
 
+Me.bind()
 
 })();
