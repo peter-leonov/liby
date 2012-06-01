90ba275cf78d62f2b00ccd589a49724a7e8829c6
Peter Leonov <gojpeg@gmail.com>
2012-06-01 23:59:20 +0400

    sync

--- src/fixes/trident4.js
+++ src/fixes/trident4.js
@@ -1,10 +1,11 @@
 <!--# include virtual="trident-3-4-prototype.js" -->
+
 <!--# include virtual="trident-3-4-event-listener.js" -->
+<!--# include virtual="trident4-element-fix.js" -->
 <!--# include virtual="trident-3-4-script-onload.js" -->
 <!--# include virtual="trident-3-4-input-focus-blur-change.js" -->
 <!--# include virtual="trident-3-4-form-submit.js" -->
 <!--# include virtual="trident-3-4-image-dragdrop.js" -->
-<!--# include virtual="trident4-element-fix.js" -->
 <!--# include virtual="trident-3-4-onready.js" -->
 <!--# include virtual="trident-3-4-window-page-offset.js" -->
 <!--# include virtual="trident4-selectors.js" -->
