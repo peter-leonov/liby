502bc686b177bef27af48139310fabdceed33515
Peter Leonov <gojpeg@gmail.com>
2012-08-03 12:45:53 +0400

    relax

--- src/modules/element.js
+++ src/modules/element.js
@@ -10,79 +10,78 @@ function toggleTo (cn, state)
 
 DOMTokenList.prototype.toggleTo = toggleTo
 
-Object.add
-(
-	Element.prototype,
+var prototype =
+{
+	empty: function ()
 	{
-		empty: function ()
-		{
-			var node
-			while (node = this.firstChild)
-				this.removeChild(node)
-		},
+		var node
+		while (node = this.firstChild)
+			this.removeChild(node)
+	},
+	
+	hide: function () { this.classList.add('hidden') },
+	show: function () { this.classList.remove('hidden') },
+	
+	remove: function ()
+	{
+		var parent = this.parentNode
+		if (!parent)
+			return
 		
-		hide: function () { this.classList.add('hidden') },
-		show: function () { this.classList.remove('hidden') },
-		
-		remove: function ()
+		parent.removeChild(this)
+	},
+	
+	isParent: function (parent, root)
+	{
+		var node = this
+		do
 		{
-			var parent = this.parentNode
-			if (!parent)
-				return
-			
-			parent.removeChild(this)
-		},
-		
-		isParent: function (parent, root)
-		{
-			var node = this
-			do
-			{
-				if (node === parent)
-					return true
-				if (node === root)
-					break
-			}
-			while ((node = node.parentNode))
-			
-			return false
-		},
-		
-		offsetPosition: function (root)
-		{
-			var node = this,
-				left = 0, top = 0
-			
-			if (node == root)
-				return {left: left, top: top}
-			
-			var parent, lastNode
-			for (;;)
-			{
-				left += node.offsetLeft
-				top += node.offsetTop
-				
-				parent = node.offsetParent
-				if (parent && parent !== root)
-				{
-					lastNode = node
-					node = parent
-				}
-				else
-				{
-					if (lastNode)
-					{
-						left -= lastNode.scrollLeft
-						top -= lastNode.scrollTop
-					}
-					
-					break
-				}
-			}
-			
-			return {left: left, top: top}
+			if (node === parent)
+				return true
+			if (node === root)
+				break
 		}
+		while ((node = node.parentNode))
+		
+		return false
+	},
+	
+	offsetPosition: function (root)
+	{
+		var node = this,
+			left = 0, top = 0
+		
+		if (node == root)
+			return {left: left, top: top}
+		
+		var parent, lastNode
+		for (;;)
+		{
+			left += node.offsetLeft
+			top += node.offsetTop
+			
+			parent = node.offsetParent
+			if (parent && parent !== root)
+			{
+				lastNode = node
+				node = parent
+			}
+			else
+			{
+				if (lastNode)
+				{
+					left -= lastNode.scrollLeft
+					top -= lastNode.scrollTop
+				}
+				
+				break
+			}
+		}
+		
+		return {left: left, top: top}
 	}
-)
+}
+
+Object.add(Element.prototype, prototype)
 
 })();
