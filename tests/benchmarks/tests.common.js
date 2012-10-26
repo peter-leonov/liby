3a9d044e22c7f55b768f04fb7ebd76332e3ea6fa
Peter Leonov <gojpeg@gmail.com>
2012-10-26 14:50:59 +0400

    fix tests in benchmark/

--- tests/benchmarks/tests.common.js
+++ tests/benchmarks/tests.common.js
@@ -3,10 +3,10 @@
 function script (src) { document.write('<script src="' + src + '"></script>') }
 function style (src) { document.write('<link rel="stylesheet" href="' + src + '"/>') }
 
-script('cascade/q.js')
-script('cascade/test.js')
-script('cascade/test-tool.js')
-script('cascade/tests.js')
+script('../../modules/q.js')
+script('../cascade/test.js')
+script('../cascade/test-tool.js')
+script('../cascade/tests.js')
 
 style('../tests.css')
 
