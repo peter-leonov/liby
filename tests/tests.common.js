ad925b2ba9a35861057363b0795f89df148023dc
Peter Leonov <gojpeg@gmail.com>
2012-10-26 14:36:49 +0400

    remove src from tests

--- tests/tests.common.js
+++ tests/tests.common.js
@@ -3,7 +3,7 @@
 function script (src) { document.write('<script src="' + src + '"></script>') }
 function style (src) { document.write('<link rel="stylesheet" href="' + src + '"/>') }
 
-script('../src/modules/q.js')
+script('../modules/q.js')
 script('cascade/test.js')
 script('cascade/test-tool.js')
 script('cascade/tests.js')
