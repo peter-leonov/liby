13260a739a96cf20d21ff3d9b5c5c4a78870882a
Peter Leonov <gojpeg@gmail.com>
2012-11-04 22:49:28 +0400

    kinda jQuery generation
    fast.li(), n.li(), node.li()

--- modules/nodes.js
+++ modules/nodes.js
@@ -1,25 +1,53 @@
 ;(function(){
 
+function NodesBuilder (node)
+{
+	this.node = node
+}
+
+var proto = NodesBuilder.prototype
+var fast = NodesBuilder.fast = {}
+
+proto.text = fast.text = function (text)
+{
+	var node = doc.createTextNode(text)
+	this.node.appendChild(node)
+	return node
+}
+
+
+var tags = ['div', 'span', 'ul', 'dl', 'li', 'dt', 'dd', 'img']
+for (var i = 0, il = tags.length; i < il; i++)
+	bakeShortcut(tags[i])
+
+
 var doc = document
 
-var prototype =
+function bakeShortcut (tag)
 {
-	add: function (tag, cn)
+	proto[tag] = function (cn)
 	{
 		var node = doc.createElement(tag)
 		node.className = cn
-		this.appendChild(node)
-		return node
-	},
+		this.node.appendChild(node)
+		return new NodesBuilder(node)
+	}
 	
-	text: function (text)
+	NodesBuilder[tag] = function (cn)
 	{
-		var node = doc.createTextNode(text)
-		this.appendChild(node)
+		var node = doc.createElement(tag)
+		node.className = cn
+		return new NodesBuilder(node)
+	}
+	
+	fast[tag] = function (cn)
+	{
+		var node = doc.createElement(tag)
+		node.className = cn
 		return node
 	}
 }
 
-Object.add(Element.prototype, prototype)
+self[NodesBuilder.name] = NodesBuilder
 
 })();
