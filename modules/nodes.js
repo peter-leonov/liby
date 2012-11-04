07015587608d6ddf2a17fd0843e9c42d741f87a1
Peter Leonov <gojpeg@gmail.com>
2012-11-04 21:03:00 +0400

    clean up

--- modules/nodes.js
+++ modules/nodes.js
@@ -1,4 +1,4 @@
-;(function(){ // new
+;(function(){
 
 var doc = document
 
@@ -21,59 +21,5 @@ var prototype =
 }
 
 Object.add(Element.prototype, prototype)
-
-})();
-
-
-;(function(){ // old
-
-var doc = document, undef, myName = 'NodesShortcut'
-
-function T (text)
-{
-	return doc.createTextNode(text)
-}
-
-function N (tag)
-{
-	return doc.createElement(tag)
-}
-
-function Nc (tag, cn)
-{
-	var node = doc.createElement(tag)
-	node.className = cn
-	return node
-}
-
-function Nct (tag, cn, text)
-{
-	var node = doc.createElement(tag)
-	node.className = cn
-	node.appendChild(doc.createTextNode(text))
-	return node
-}
-
-
-function E (tag, cn, props)
-{
-	var node = doc.createElement(tag)
-	if (cn !== undef) node.className = cn
-	if (props)
-		for (var i in props)
-			node.setAttribute(i, props[i])
-	return node
-}
-
-var Me = self[myName] = {}
-	funcs = Me.funcs = {T: T, N: N, Nc: Nc, Nct: Nct, E: E}
-
-var pairs = []
-for (var k in funcs)
-	pairs.push(k + '=' + myName + '.funcs.' + k)
-
-Me.code = 'var ' + pairs.join(',')
-Me.include = function () { return this.code }
-
 
 })();
