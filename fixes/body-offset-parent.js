4c1adce6a4b32604b98dce1a5a77e3d864e49344
Peter Leonov <gojpeg@gmail.com>
2013-01-04 00:48:31 +0400

    definitely garbage collected

--- fixes/body-offset-parent.js
+++
@@ -1,2 +0,0 @@
-if (/Opera\/(9\.[5-9]|10\.)/.test(navigator.userAgent))
-	document.body.offsetParent = document.documentElement
--- fixes/timed-redraw.js
+++
@@ -1,3 +0,0 @@
-;(function(){
-window.setInterval(function () { if (document.body) document.body.className = document.body.className }, 2000)
-})();
