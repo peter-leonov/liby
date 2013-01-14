548872061efd1eaf0d96e410dbaf262ae9ed3bca
Peter Leonov <gojpeg@gmail.com>
2013-01-14 15:03:27 +0400

    make it work

--- tests/test-framework/tests.common.js
+++ tests/test-framework/tests.common.js
@@ -3,11 +3,11 @@
 function script (src) { document.write('<script src="' + src + '"></script>') }
 function style (src) { document.write('<link rel="stylesheet" href="' + src + '"/>') }
 
-script('../modules/q.js')
-script('cascade/test.js')
-script('cascade/test-tool.js')
-script('cascade/tests.js')
+script('../test-framework/test.js')
+script('../test-framework/test-q.js')
+script('../test-framework/test-tool.js')
+script('../test-framework/tests.js')
 
-style('tests.css')
+style('../test-framework/tests.css')
 
 })();
