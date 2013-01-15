0b2404e04ea08ad0822bf5c4be293517a06c616c
Peter Leonov <gojpeg@gmail.com>
2013-01-15 17:14:00 +0400

    no cache

--- modules/cosy.js
+++ modules/cosy.js
@@ -13,17 +13,12 @@ $.id = function (id) { return document.getElementById(id) }
 
 $.onload = function (f) { return window.addEventListener('load', f, false) }
 $.onready = function (f) { document.addEventListener('DOMContentLoaded', f, false) }
-$.load = function (src)
+$.script = function (src)
 {
-	var me = arguments.callee
-	var cache = me.cache || (me.cache = {})
-	if (me.cache[src])
-		return me.cache[src]
 	var node = document.createElement('script')
 	node.type = 'text/javascript'
 	node.src = src
 	document.getElementsByTagName('head')[0].appendChild(node)
-	cache[src] = node
 	return node
 }
 
