73e076404a9b9f33f70530f86d612af219fc6dd5
Peter Leonov <gojpeg@gmail.com>
2013-01-15 17:05:20 +0400

    simple post

--- modules/request.js
+++ modules/request.js
@@ -2,27 +2,14 @@
 
 var Request =
 {
-	charset: 'utf-8',
-	post: function (url, data, callback, sync)
-	{
-		var r = new XMLHttpRequest()
-		
-		r.open('POST', url, !sync)
-		r.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=' + this.charset)
-		if (!sync)
-			r.onreadystatechange = function () { onreadystatechange.call(r) } // wrapped for FF 2.0
-		if (callback)
-			r.callback = callback
-		r.send(data)
-		if (sync)
-			onreadystatechange.call(r)
-		
-		return r
-	},
-	
 	get: function (url, callback)
 	{
 		return Request.open('GET', url, null, callback)
+	},
+	
+	post: function (url, data, callback)
+	{
+		return Request.open('POST', url, data, callback)
 	},
 	
 	open: function (method, url, data, callback)
