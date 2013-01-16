02abdeaa67c5d319d92cbbe88463af2e34d82d21
Peter Leonov <gojpeg@gmail.com>
2013-01-16 21:43:50 +0400

    Liby.DateDiff

--- modules/date-diff.js
+++ modules/date-diff.js
@@ -1,17 +1,5 @@
 ;(function(){
 
-var D = Date
-
-D.second = 1000
-D.minute = 6e4
-D.hour = 36e5
-D.day = 864e5
-D.week = 7 * 864e5
-D.year = 365 * 864e5
-
-if (!D.now)
-	D.now = function () { return +new D() }
-
 var map =
 {
 	s: 'second',
@@ -22,29 +10,36 @@ var map =
 	y: 'year'
 }
 
-D.parseDiff = function (str)
+var DateDiff =
 {
-	var m = /^([+-])?(\d+(?:\.\d+)?)([smhdwy])?$/.exec(str)
-	if (!m)
-		return null
+	second: 1000,
+	minute: 6e4,
+	hour: 36e5,
+	day: 864e5,
+	week: 7 * 864e5,
+	year: 365 * 864e5,
 	
-	return [m[1] || '+', +m[2], map[m[3] || 's']]
+	parse: function (str)
+	{
+		var m = /^([+-])?(\d+(?:\.\d+)?)([smhdwy])?$/.exec(str)
+		if (!m)
+			return null
+		
+		return [m[1] || '+', +m[2], map[m[3] || 's']]
+	},
+	
+	compute: function (diff)
+	{
+		var s = Math.round(diff[1] * DateDiff[diff[2]])
+		return diff[0] == '-' ? -s : s
+	},
+	
+	add: function (date, str)
+	{
+		return new Date(+date + DateDiff.compute(DateDiff.parse(str)))
+	}
 }
 
-D.computeDiff = function (diff)
-{
-	var s = Math.round(diff[1] * D[diff[2]])
-	return diff[0] == '-' ? -s : s
-}
-
-D.prototype.add = function (str)
-{
-	return new Date(+this + D.computeDiff(D.parseDiff(str)))
-}
-
-D.add = function (str)
-{
-	return new D().add(str)
-}
+Liby.DateDiff = DateDiff
 
 })();
