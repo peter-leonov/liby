4ea8e56c395b0f6ed91c6a9965e0dca9adba4a32
Peter Leonov <gojpeg@gmail.com>
2013-01-17 22:55:05 +0400

    pub shuffle methods

--- public.js
+++ public.js
@@ -1,4 +1,6 @@
+// animation.js
 Element.prototype.animate = function () { return Liby.Animation.animate(this, motion, props, duration, unit) }
 
-Array.prototype.randomize = function () { return Liby.ArrayRandom.randomize(this) }
-Array.prototype.random = function (n) { return Liby.ArrayRandom.random(this, n) }
+// array-shuffle.js
+Array.prototype.random = Liby.ArrayRandom.random
+Array.prototype.shuffle = Liby.ArrayRandom.shuffle
