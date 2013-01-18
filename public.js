783dc615e0c6f6a063907741cdbb1f38f4627ee1
Peter Leonov <gojpeg@gmail.com>
2013-01-18 21:36:19 +0400

    untested garbage collected

--- public.js
+++ public.js
@@ -1,6 +1,3 @@
-// animation.js
-Element.prototype.animate = Liby.Animation.animate
-
 // array-shuffle.js
 Array.prototype.random = Liby.ArrayRandom.random
 Array.prototype.shuffle = Liby.ArrayRandom.shuffle
